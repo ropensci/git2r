4d08f018c092ad1de0db21e3fdceb83aac5ffdee
Stefan Widgren <stefan.widgren@gmail.com>
2013-12-30 21:02:54 +0100

    Added S4 class git_signature

---
+++ R/signature.r
@@ -0,0 +1,44 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##' Class \code{"git_signature"}
+##'
+##' S4 class to handle a git signature
+##' @section Slots:
+##' \describe{
+##'   \item{name}{
+##'     The full name of the author.
+##'   }
+##'   \item{email}{
+##'     Email of the author.
+##'   }
+##'   \item{when}{
+##'     Time when the action happened.
+##'   }
+##' }
+##' @name git_signature-class
+##' @aliases show,git_signature-method
+##' @docType class
+##' @keywords classes
+##' @section Methods:
+##' \describe{
+##'   \item{show}{\code{signature(object = "git_signature")}}
+##' }
+##' @keywords methods
+##' @export
+setClass('git_signature',
+         slots=c(name='character',
+                 email='character',
+                 when='character'))
