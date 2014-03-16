771294a0650045c872723d3b68dd8ccd641852c0
Karthik Ram <karthik.ram@gmail.com>
2014-03-16 13:51:31 -0700

    Updated documentation and added a new function for markdown_link

---
+++ R/markdown_link.r
@@ -0,0 +1,17 @@
+#' Generate a markdown link for current commit
+#'
+#' This function would be ideal to use when writing manscripts with markdown. 
+#' By adding a bit of text like "All the files at the current state of the manuscript can by found in commit" followed by \code{markdown_link(repo)},
+#' One can maintain some degree of provenance.
+#' @param repo Path to local git repository
+#' @export 
+#' @examples \dontrun{
+#' markdown_link(".")
+#'}
+markdown_link <- function(repo) {
+repo <- repository(repo)	
+commit <- git2r::head(repo)@hex
+short_hash <- substr(commit, 1, 6)
+remote_link <- paste0(remote_url(repo), "/commit/", commit)
+sprintf("[%s](%s)", short_hash, remote_link)
+}
