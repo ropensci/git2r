44a9cafb753e6b2da5f073110999aad249b66e28
Stefan Widgren <stefan.widgren@gmail.com>
2014-03-22 11:50:55 +0100

    Merged functionality of contributions functions
    * R/contributions.r (contributions, contributions_by_user): Merged the
      two functions into one 'contributions'. Added one argument 'by' to
      determine what to group by.
    
    * Changed code style to be consistent with other code
    
    * Added GPLv2 header

--- R/contributions.r
+++ R/contributions.r
@@ -1,65 +1,58 @@
-#' Contributions_per_month
-#'
-#' Generates a ggplot chart of number of contributions to git repo per month
-#' @param dir Path to git repo. Default is current working directory.
-#' @param  title Default title for the plot is commits. Supply a new title if you desire one.
-#' @param  breaks Default is \code{month}. Change to week or day as necessary.
-#' @return A \code{data.frame} with contributions.
-#' @importFrom scales date_format
-#' @import ggplot2
-#' @importFrom plyr ddply
-#' @export
-#' @author Karthik Ram \email{karthik.ram@@gmail.com}
-#' @examples \dontrun{
-#' # If current working dir is a git repo
-#' # contributions()
-#' # contributions(breaks = "week")
-#' # contributions(breaks = "day")
-#' # If the path is somewhere else
-#' # contributions(dir = "/path/to/repo")
-#' # If you need just the data, not the plot
-#' contributions(data_only = TRUE)
-#'}
-contributions <- function(dir = getwd(), breaks = 'month') {
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##' Contributions
+##'
+##' See contributions to a Git repo
+##' @param dir Path to git repo. Default is current working directory.
+##' @param breaks Default is \code{month}. Change to week or day as necessary.
+##' @param by Contributions by 'commits' or 'user'. Default is 'commits'.
+##' @return A \code{data.frame} with contributions.
+##' @importFrom plyr ddply
+##' @importFrom reshape2 melt dcast
+##' @export
+##' @author Karthik Ram \email{karthik.ram@@gmail.com}
+##' @author Stefan Widgren \email{stefan.widgren@@gmail.com}
+##' @examples \dontrun{
+##' # If current working dir is a git repo
+##' # contributions()
+##' # contributions(breaks = "week")
+##' # contributions(breaks = "day")
+##' # If the path is somewhere else
+##' # contributions(dir = "/path/to/repo")
+##'}
+contributions <- function(dir = getwd(),
+                          breaks = c('month', 'week', 'day'),
+                          by = c('commits', 'user'))
+{
+    breaks <- match.arg(breaks)
+    by <- match.arg(by)
+
     repo <- repository(dir)
     df <- do.call('rbind', lapply(commits(repo), function(x) {
         data.frame(name = x@author@name,
                    when = as(x@author@when, 'POSIXct'))
     }))
 
-    ## Format data
-    df$month <- as.POSIXct(cut(df$when, breaks = breaks))
-    df <- ddply(df, ~month, nrow)
-    names(df) <- c('month', 'n')
-
-    df
-}
-
-
-#' contribution_by_user
-#'
-#' See contributions to a Git repo by user
-#' @param dir Path to git repo. Default is current working directory.
-#' @param  breaks Default is \code{month}. Change to week or day as necessary.
-#' @return A \code{data.frame} with contribtions by user.
-#' @import dplyr
-#' @importFrom scales date_format
-#' @import ggplot2
-#' @importFrom reshape2 melt dcast
-#' @export
-#' @author Karthik Ram \email{karthik.ram@@gmail.com}
-#' @examples \dontrun{
-#' contribution_by_user()
-#' # To just download the data to use separately
-#' contribution_by_user(data_only = TRUE)
-#'}
-contribution_by_user <- function(dir = getwd(), breaks = 'month') {
-    repo <- repository(dir)
-    df <- do.call('rbind', lapply(commits(repo), function(x) {
-        data.frame(name = x@author@name,
-                   when = as(x@author@when, 'POSIXct'))
-    }))
-    df$month <- as.POSIXct(cut(df$when, breaks = breaks))
+    if(identical(by, 'commits')) {
+        ## Format data
+        df$when <- as.POSIXct(cut(df$when, breaks = breaks))
+        df <- ddply(df, ~when, nrow)
+        names(df) <- c('when', 'n')
+        return(df)
+    }
 
     ## Summarise the results
     df_summary <- df %.%
