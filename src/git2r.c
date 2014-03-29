7dc5f179cf702b5c32b3e1c6753f5c7ead4be002
Stefan Widgren <stefan.widgren@gmail.com>
2014-03-29 17:27:12 +0100

    Refactoring of function to list tags
    * src/git2r.c (tags): Refactored function to list tags
    
    * src/git2r.c (init_tag): New function to initialize a S4 class
      git_tag object

--- src/git2r.c
+++ src/git2r.c
@@ -624,6 +624,45 @@ static void init_signature(const git_signature *sig, SEXP signature)
 }
 
 /**
+ * Init slots in S4 class git_tag
+ *
+ * @param source a tag
+ * @param dest S4 class git_tag to initialize
+ * @return void
+ */
+static void init_tag(git_tag *source, SEXP dest)
+{
+    int err;
+    const git_signature *tagger;
+    const git_oid *oid;
+    char target[GIT_OID_HEXSZ + 1];
+
+    SET_SLOT(dest,
+             Rf_install("message"),
+             ScalarString(mkChar(git_tag_message(source))));
+
+    SET_SLOT(dest,
+             Rf_install("name"),
+             ScalarString(mkChar(git_tag_name(source))));
+
+    tagger = git_tag_tagger(source);
+    if (tagger) {
+        SEXP sexp_tagger;
+
+        PROTECT(sexp_tagger = NEW_OBJECT(MAKE_CLASS("git_signature")));
+        init_signature(tagger, sexp_tagger);
+        SET_SLOT(dest, Rf_install("tagger"), sexp_tagger);
+        UNPROTECT(1);
+    }
+
+    oid = git_tag_target_id(source);
+    git_oid_tostr(target, sizeof(target), oid);;
+    SET_SLOT(dest,
+             Rf_install("target"),
+             ScalarString(mkChar(target)));
+}
+
+/**
  * Check if repository is bare.
  *
  * @param repo S4 class git_repository
@@ -1006,93 +1045,75 @@ SEXP state(const SEXP repo)
  * Get all tags that can be found in a repository.
  *
  * @param repo S4 class git_repository
- * @return VECXSP with S4 objects of class tag
+ * @return VECXSP with S4 objects of class git_tag
  */
 SEXP tags(const SEXP repo)
 {
-    int i, err;
-    const char* err_msg = NULL;
-    git_strarray tag_list;
+    int err;
     SEXP list;
-    SEXP names;
     size_t protected = 0;
-    const git_signature *signature;
     git_repository *repository;
+    git_reference* reference = NULL;
+    git_tag *tag = NULL;
+    git_strarray tag_names = {0};
+    size_t i;
 
     repository = get_repository(repo);
     if (!repository)
         error(err_invalid_repository);
 
-    err = git_tag_list(&tag_list, repository);
+    err = git_tag_list(&tag_names, repository);
     if (err < 0)
         goto cleanup;
 
-    PROTECT(list = allocVector(VECSXP, tag_list.count));
-    PROTECT(names = allocVector(STRSXP, tag_list.count));
-    protected = 2;
+    PROTECT(list = allocVector(VECSXP, tag_names.count));
+    protected++;
 
-    for (i = 0; i < tag_list.count; i++) {
-        char* buf;
-        git_reference *ref;
-        SEXP tag;
-        char *tagname;
-        size_t tagname_len;
+    for(i = 0; i < tag_names.count; i++) {
+        SEXP sexp_tag;
+        const git_oid *oid;
 
-        PROTECT(tag = NEW_OBJECT(MAKE_CLASS("git_tag")));
+        err = git_reference_dwim(&reference, repository, tag_names.strings[i]);
+        if (err < 0)
+            goto cleanup;
+
+        oid = git_reference_target(reference);
+        err = git_tag_lookup(&tag, repository, oid);
+        if (err < 0)
+            goto cleanup;
+
+        PROTECT(sexp_tag = NEW_OBJECT(MAKE_CLASS("git_tag")));
         protected++;
+        init_tag(tag, sexp_tag);
 
-        tagname = tag_list.strings[i];
-
-        /* Prefix tagname with "refs/tags/" to lookup reference */
-        tagname_len = strlen(tagname);
-        buf = malloc((tagname_len+11)*sizeof(char));
-        if (!buf) {
-            err = -1;
-            err_msg = err_alloc_memory_buffer;
-            goto cleanup;
-        }
-        *buf = '\0';
-        strncat(buf, "refs/tags/", 10);
-        strncat(buf, tagname, tagname_len);
-        err = git_reference_lookup(&ref, repository, buf);
-        if (err < 0) {
-            free(buf);
-            goto cleanup;
-        }
-        free(buf);
-        init_reference(ref, tag);
-
-        /* Fill in signature for tag */
-        /* oid = git_reference_target(ref); */
-        /* if (oid) { */
-        /*     err = git_tag_lookup(&t, repository, oid); */
-        /*     signature = git_tag_tagger(t); */
-        /*     SET_SLOT(GET_SLOT(tag, Rf_install("sig")), */
-        /*              Rf_install("name"), */
-        /*              ScalarString(mkChar(signature->name))); */
-        /* } */
-
-        SET_STRING_ELT(names, i, mkChar(tagname));
-        SET_VECTOR_ELT(list, i, tag);
+        SET_VECTOR_ELT(list, i, sexp_tag);
         UNPROTECT(1);
         protected--;
+
+        git_tag_free(tag);
+        tag = NULL;
+        git_reference_free(reference);
+        reference = NULL;
     }
 
 cleanup:
-    git_strarray_free(&tag_list);
-    git_repository_free(repository);
+    git_strarray_free(&tag_names);
 
-    setAttrib(list, R_NamesSymbol, names);
+    if (tag)
+        git_tag_free(tag);
+
+    if (reference)
+        git_reference_free(reference);
+
+    if (repository)
+        git_repository_free(repository);
+
     if (protected)
         UNPROTECT(protected);
 
     if (err < 0) {
-        if (err_msg) {
-            error(err_msg);
-        } else {
-            const git_error *e = giterr_last();
-            error("Error %d: %s\n", e->klass, e->message);
-        }
+        const git_error *e = giterr_last();
+        error("Error %d: %s\n", e->klass, e->message);
     }
 
     return list;
