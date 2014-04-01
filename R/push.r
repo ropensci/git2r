dc35b4064fe5cca0544ceb5eb5598d72701509e9
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-01 21:41:24 +0200

    Initial work on push functionality
    * R/push.r (push): Added file with generic method 'push' and method
      for a S4 class git_repository
    
    * man/push-methods.Rd: Added file
    
    * Updated NAMESPACE and DESCRIPTION

---
+++ R/push.r
@@ -0,0 +1,37 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##' Push
+##'
+##' @rdname push-methods
+##' @docType methods
+##' @param repo the repository
+##' @param refspec the refspec to be pushed
+##' @return invisible(NULL)
+##' @keywords methods
+##' @include repository.r
+setGeneric('push',
+           signature = 'repo',
+           function(repo, refspec) standardGeneric('push'))
+
+##' @rdname push-methods
+##' @export
+setMethod('push',
+          signature(repo = 'git_repository'),
+          function (repo, refspec)
+          {
+              invisible(NULL)
+          }
+)
