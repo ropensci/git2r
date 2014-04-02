1e30f80f76391b28650db92fc931897071cd1b88
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-02 21:05:33 +0200

    Added method when to facilitate extraction of time
    * R/when.r (when): Added generic method when to facilitate extraction
      of the time slot as a character vector from the S4 classes
      git_commit, git_signature, git_tag and git_time.
    
    * tests/when.R: Test cases to test method when
    
    * man/when-methods.Rd: Added
    
    * Updated NAMESPACE and DESCRIPTION

---
+++ R/when.r
@@ -0,0 +1,79 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##' When
+##'
+##' Help method to extract the time as a character string from the S4
+##' classes \code{git_commit}, \code{git_signature}, \code{git_tag}
+##' and \code{git_time}.
+##' @rdname when-methods
+##' @docType methods
+##' @param object the \code{object} to extract the time slot from.
+##' @return A \code{character} vector of length one.
+##' @keywords methods
+##' @include commit.r
+##' @include signature.r
+##' @include tag.r
+##' @include time.r
+##' @examples \dontrun{
+##' ## Open an existing repository
+##' repo <- repository('path/to/git2r')
+##'
+##' when(commits(repo)[[1]])
+##'}
+setGeneric('when',
+           signature = 'object',
+           function(object)
+           standardGeneric('when'))
+
+##' @rdname when-methods
+##' @export
+setMethod('when',
+          signature(object = 'git_commit'),
+          function (object)
+          {
+              as(object@author@when, 'character')
+          }
+)
+
+##' @rdname when-methods
+##' @export
+setMethod('when',
+          signature(object = 'git_signature'),
+          function (object)
+          {
+              as(object@when, 'character')
+          }
+)
+
+##' @rdname when-methods
+##' @export
+setMethod('when',
+          signature(object = 'git_tag'),
+          function (object)
+          {
+              as(object@tagger@when, 'character')
+          }
+)
+
+##' @rdname when-methods
+##' @export
+setMethod('when',
+          signature(object = 'git_time'),
+          function (object)
+          {
+              as(object, 'character')
+          }
+)
---
+++ tests/when.R
@@ -0,0 +1,52 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library(git2r)
+
+##
+## Check when method
+##
+stopifnot(identical(when(new('git_commit',
+                             hex = '166f3f779fd7e4165aaa43f2828050ce040052b0',
+                             author = new('git_signature',
+                                 name = 'Stefan Widgren',
+                                 email = 'stefan.widgren@gmail.com',
+                                 when = new('git_time', time = 1395567947, offset = 60)),
+                             committer = new('git_signature',
+                                 name = 'Stefan Widgren',
+                                 email = 'stefan.widgren@gmail.com',
+                                 when = new('git_time', time = 1395567950, offset = 60)),
+                             summary = 'A commit summary',
+                             message = 'A commit message')),
+                    "2014-03-23 10:45:47"))
+
+stopifnot(identical(when(new('git_signature',
+                             name = 'Stefan Widgren',
+                             email = 'stefan.widgren@gmail.com',
+                             when = new('git_time', time = 1395567947, offset = 60))),
+                    "2014-03-23 10:45:47"))
+
+stopifnot(identical(when(new('git_tag',
+                             message = 'A tag message',
+                             name = 'A tage name',
+                             tagger = new('git_signature',
+                                 name = 'Stefan Widgren',
+                                 email = 'stefan.widgren@gmail.com',
+                                 when = new('git_time', time = 1395567947, offset = 60)),
+                             target = '166f3f779fd7e4165aaa43f2828050ce040052b0')),
+                    "2014-03-23 10:45:47"))
+
+stopifnot(identical(when(new('git_time', time = 1395567947, offset = 60)),
+                    "2014-03-23 10:45:47"))
