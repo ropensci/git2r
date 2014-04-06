0c6cbab5cf48a0b8fa6bc102273c5aa7d374d4aa
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-06 21:56:12 +0200

    Updated/added GPLv2 header of each git2r file
    * git2r authors are listed in DESCRIPTION
    
    * Added FSF address

--- R/markdown_link.r
+++ R/markdown_link.r
@@ -1,18 +1,34 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
  #' @include commit.r
 #' @name markdown_link
 #'
 #' @title Generate a markdown link for current commit
 #'
-#' @description This function would be ideal to use when writing manscripts with markdown. 
+#' @description This function would be ideal to use when writing manscripts with markdown.
 #' By adding a bit of text like "All the files at the current state of the manuscript can by found in commit" followed by \code{markdown_link(repo)},
 #' One can maintain some degree of provenance.
 #' @param repo Path to local git repository
-#' @export 
+#' @export
 #' @examples \dontrun{
 #' markdown_link(".") # or path to repo
 #'}
 markdown_link <- function(repo = ".") {
-repo <- repository(repo)	
+repo <- repository(repo)
 commit <- git2r::head(repo)@hex
 short_hash <- substr(commit, 1, 6)
 remote_link <- paste0(remote_url(repo), "/commit/", commit)
--- R/tree.r
+++ R/tree.r
@@ -1,17 +1,18 @@
 ## git2r, R bindings to the libgit2 library.
-## Copyright (C) 2013-2014  Stefan Widgren
+## Copyright (C) 2013-2014 The git2r contributors
 ##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, version 2 of the License.
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
 ##
 ## git2r is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ##' Class \code{"git_tree"}
 ##'
