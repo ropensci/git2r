bf0afd20504007a945f8d023a67be5d0125afd9b
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-06 15:14:47 +0200

    Added test cases for status method

---
+++ tests/status.R
@@ -0,0 +1,128 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2 of the License.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library(git2r)
+
+##
+## Create a directory in tempdir
+##
+path <- tempfile(pattern="git2r-")
+dir.create(path)
+
+##
+## Initialize a repository
+##
+repo <- init(path)
+config(repo, user.name="Repo", user.email="repo@example.org")
+
+##
+## Status case 1
+##
+status_exp_1 <- structure(list(staged = structure(list(), .Names = character(0)),
+                               unstaged = structure(list(), .Names = character(0)),
+                               untracked = structure(list(), .Names = character(0))),
+                          .Names = c("staged", "unstaged", "untracked"))
+status_obs_1 <- status(repo)
+str(status_exp_1)
+str(status_obs_1)
+stopifnot(identical(status_obs_1, status_exp_1))
+
+##
+## Status case 2, include ignored files
+##
+status_exp_2 <- structure(list(staged = structure(list(), .Names = character(0)),
+                               unstaged = structure(list(), .Names = character(0)),
+                               untracked = structure(list(), .Names = character(0)),
+                               ignored = structure(list(), .Names = character(0))),
+                          .Names = c("staged", "unstaged", "untracked", "ignored"))
+status_obs_2 <- status(repo, ignored=TRUE)
+str(status_exp_2)
+str(status_obs_2)
+stopifnot(identical(status_obs_2, status_exp_2))
+
+##
+## Create 4 files
+##
+writeLines("File-1", file.path(path, "test-1.txt"))
+writeLines("File-2", file.path(path, "test-2.txt"))
+writeLines("File-3", file.path(path, "test-3.txt"))
+writeLines("File-4", file.path(path, "test-4.txt"))
+
+##
+## Status case 3: 4 untracked files
+##
+status_exp_3 <- structure(list(staged = structure(list(), .Names = character(0)),
+                               unstaged = structure(list(), .Names = character(0)),
+                               untracked = structure(list(
+                                   untracked = "test-1.txt",
+                                   untracked = "test-2.txt",
+                                   untracked = "test-3.txt",
+                                   untracked = "test-4.txt"),
+                                   .Names = c("untracked", "untracked", "untracked", "untracked"))),
+                          .Names = c("staged", "unstaged", "untracked"))
+status_obs_3 <- status(repo)
+str(status_exp_3)
+str(status_obs_3)
+stopifnot(identical(status_obs_3, status_exp_3))
+
+##
+## Add file 1 and 2 to the repository and commit
+##
+add(repo, c("test-1.txt", "test-2.txt"))
+commit(repo, "Commit message")
+
+##
+## Status case 4: 2 untracked files
+##
+status_exp_4 <- structure(list(staged = structure(list(), .Names = character(0)),
+                               unstaged = structure(list(), .Names = character(0)),
+                               untracked = structure(list(
+                                   untracked = "test-3.txt",
+                                   untracked = "test-4.txt"),
+                                   .Names = c("untracked", "untracked"))),
+                          .Names = c("staged", "unstaged", "untracked"))
+status_obs_4 <- status(repo)
+str(status_exp_4)
+str(status_obs_4)
+stopifnot(identical(status_obs_4, status_exp_4))
+
+##
+## Update file 1 & 2
+##
+writeLines(c("File-1", "Hello world"), file.path(path, "test-1.txt"))
+writeLines(c("File-2", "Hello world"), file.path(path, "test-2.txt"))
+
+##
+## Add file 1
+##
+add(repo, "test-1.txt")
+
+##
+## Status case 5: 1 staged file, 1 unstaged file and 2 untracked files
+##
+status_exp_5 <- structure(list(staged = structure(list(modified = "test-1.txt"), .Names = "modified"),
+                               unstaged = structure(list(modified = "test-2.txt"), .Names = "modified"),
+                               untracked = structure(list(untracked = "test-3.txt", untracked = "test-4.txt"),
+                                   .Names = c("untracked", "untracked"))),
+                          .Names = c("staged", "unstaged", "untracked"))
+status_obs_5 <- status(repo)
+str(status_exp_5)
+str(status_obs_5)
+stopifnot(identical(status_obs_5, status_exp_5))
+
+##
+## Cleanup
+##
+unlink(path, recursive=TRUE)
