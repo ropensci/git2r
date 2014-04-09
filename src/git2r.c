1e15f5f9d86f0d5c0843b5e42eaaac3f1a871478
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-09 21:33:26 +0200

    Moved git2r signature C code to separate file
    * Refactoring to split git2r.c into several smaller files

--- src/git2r.c
+++ src/git2r.c
@@ -29,17 +29,16 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
-#include <git2.h>
-#include <git2/repository.h>
+#include "git2.h"
 
-#include "error.h"
+#include "git2r_error.h"
+#include "git2r_repository.h"
+#include "git2r_signature.h"
 
 static size_t count_staged_changes(git_status_list *status_list);
 static size_t count_unstaged_changes(git_status_list *status_list);
-static git_repository* get_repository(const SEXP repo);
 static void init_commit(const git_commit *commit, SEXP sexp_commit);
 static void init_reference(git_reference *ref, SEXP reference);
-static void init_signature(const git_signature *sig, SEXP signature);
 static int number_of_branches(git_repository *repo, int flags, size_t *n);
 
 /**
@@ -759,47 +758,6 @@ static size_t count_untracked_files(git_status_list *status_list)
 }
 
 /**
- * Get the configured signature for a repository
- *
- * @param repo S4 class git_repository
- * @return S4 class git_signature
- */
-SEXP default_signature(const SEXP repo)
-{
-    int err;
-    git_repository *repository = NULL;
-    git_signature *signature = NULL;
-    SEXP sig;
-
-    repository = get_repository(repo);
-    if (!repository)
-        error(git2r_err_invalid_repository);
-
-    err = git_signature_default(&signature, repository);
-    if (err < 0)
-        goto cleanup;
-
-    PROTECT(sig = NEW_OBJECT(MAKE_CLASS("git_signature")));
-    init_signature(signature, sig);
-
-cleanup:
-    if (repository)
-        git_repository_free(repository);
-
-    if (signature)
-        git_signature_free(signature);
-
-    UNPROTECT(1);
-
-    if (err < 0) {
-        const git_error *e = giterr_last();
-        error("Error %d/%d: %s\n", err, e->klass, e->message);
-    }
-
-    return sig;
-}
-
-/**
  * Fetch
  *
  * @param repo
@@ -847,7 +805,7 @@ cleanup:
  * @param repo S4 class git_repository
  * @return a git_repository pointer on success else NULL
  */
-static git_repository* get_repository(const SEXP repo)
+git_repository* get_repository(const SEXP repo)
 {
     SEXP class_name;
     SEXP path;
@@ -993,36 +951,6 @@ static void init_reference(git_reference *ref, SEXP reference)
     default:
         error("Unexpected reference type");
     }
-}
-
-/**
- * Init slots in S4 class git_signature.
- *
- * @param sig
- * @param signature
- * @return void
- */
-static void init_signature(const git_signature *sig, SEXP signature)
-{
-    SEXP when;
-
-    SET_SLOT(signature,
-             Rf_install("name"),
-             ScalarString(mkChar(sig->name)));
-
-    SET_SLOT(signature,
-             Rf_install("email"),
-             ScalarString(mkChar(sig->email)));
-
-    when = GET_SLOT(signature, Rf_install("when"));
-
-    SET_SLOT(when,
-             Rf_install("time"),
-             ScalarReal((double)sig->when.time));
-
-    SET_SLOT(when,
-             Rf_install("offset"),
-             ScalarReal((double)sig->when.offset));
 }
 
 /**
---
+++ src/git2r_signature.c
@@ -0,0 +1,92 @@
+/*
+ *  git2r, R bindings to the libgit2 library.
+ *  Copyright (C) 2013-2014 The git2r contributors
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, version 2,
+ *  as published by the Free Software Foundation.
+ *
+ *  git2r is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "git2r_error.h"
+#include "git2r_repository.h"
+#include "git2r_signature.h"
+
+/**
+ * Get the configured signature for a repository
+ *
+ * @param repo S4 class git_repository
+ * @return S4 class git_signature
+ */
+SEXP default_signature(const SEXP repo)
+{
+    int err;
+    git_repository *repository = NULL;
+    git_signature *signature = NULL;
+    SEXP sig;
+
+    repository = get_repository(repo);
+    if (!repository)
+        error(git2r_err_invalid_repository);
+
+    err = git_signature_default(&signature, repository);
+    if (err < 0)
+        goto cleanup;
+
+    PROTECT(sig = NEW_OBJECT(MAKE_CLASS("git_signature")));
+    init_signature(signature, sig);
+
+cleanup:
+    if (repository)
+        git_repository_free(repository);
+
+    if (signature)
+        git_signature_free(signature);
+
+    UNPROTECT(1);
+
+    if (err < 0) {
+        const git_error *e = giterr_last();
+        error("Error %d/%d: %s\n", err, e->klass, e->message);
+    }
+
+    return sig;
+}
+
+/**
+ * Init slots in S4 class git_signature.
+ *
+ * @param sig
+ * @param signature
+ * @return void
+ */
+void init_signature(const git_signature *sig, SEXP signature)
+{
+    SEXP when;
+
+    SET_SLOT(signature,
+             Rf_install("name"),
+             ScalarString(mkChar(sig->name)));
+
+    SET_SLOT(signature,
+             Rf_install("email"),
+             ScalarString(mkChar(sig->email)));
+
+    when = GET_SLOT(signature, Rf_install("when"));
+
+    SET_SLOT(when,
+             Rf_install("time"),
+             ScalarReal((double)sig->when.time));
+
+    SET_SLOT(when,
+             Rf_install("offset"),
+             ScalarReal((double)sig->when.offset));
+}
