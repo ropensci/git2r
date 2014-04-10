486f1f3573f7beb1009064f700cda588fa56e19d
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-10 21:00:57 +0200

    Moved git2r checkout C code to separate file
    * Refactoring to split git2r.c into several smaller files
    
    * src/git2r_checkout.h: Added
    
    * src/git2r_checkout.c: Added

--- src/git2r.c
+++ src/git2r.c
@@ -31,6 +31,7 @@
 
 #include "git2.h"
 
+#include "git2r_checkout.h"
 #include "git2r_clone.h"
 #include "git2r_commit.h"
 #include "git2r_error.h"
@@ -39,8 +40,6 @@
 #include "git2r_signature.h"
 #include "git2r_status.h"
 
-static size_t count_staged_changes(git_status_list *status_list);
-static size_t count_unstaged_changes(git_status_list *status_list);
 static void init_reference(git_reference *ref, SEXP reference);
 static int number_of_branches(git_repository *repo, int flags, size_t *n);
 
@@ -226,84 +225,6 @@ cleanup:
     }
 
     return list;
-}
-
-/**
- * Checkout
- *
- * @param repo S4 class git_repository
- * @param treeish
- * @return R_NilValue
- */
-SEXP checkout(SEXP repo, SEXP treeish)
-{
-    enum CHECKOUT_ACTION {
-        CHECKOUT_COMMIT,
-        CHECKOUT_TAG,
-        CHECKOUT_TREE,
-        CHECKOUT_HEAD
-    } checkout_action;
-
-    int err;
-    git_repository *repository = NULL;
-    git_checkout_opts checkout_opts = GIT_CHECKOUT_OPTS_INIT;
-    checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
-
-    /* Check arguments to checkout */
-    if (R_NilValue == repo || R_NilValue == treeish)
-        error(git2r_err_invalid_checkout_args);
-
-    /* Determine checkout strategy */
-    if(S4SXP == TYPEOF(treeish)) {
-        const char *class_name = CHAR(STRING_ELT(getAttrib(treeish, R_ClassSymbol), 0));
-
-        if (0 == strcmp(class_name, "git_commit"))
-            checkout_action = CHECKOUT_COMMIT;
-        else if(0 == strcmp(class_name, "git_tag"))
-            checkout_action = CHECKOUT_TAG;
-        else if(0 == strcmp(class_name, "git_tree"))
-            checkout_action = CHECKOUT_TREE;
-        else
-            error(git2r_err_invalid_checkout_args);
-    } else if (isString(treeish)
-               && 1 == length(treeish)
-               && 0 == strcmp(CHAR(STRING_ELT(treeish, 0)), "HEAD")) {
-        checkout_action = CHECKOUT_HEAD;
-    } else {
-        error(git2r_err_invalid_checkout_args);
-    }
-
-    repository = get_repository(repo);
-    if (!repository)
-        error(git2r_err_invalid_repository);
-
-    switch (checkout_action) {
-    case CHECKOUT_COMMIT:
-        /* :TODO:FIX: */
-        break;
-    case CHECKOUT_TAG:
-        /* :TODO:FIX: */
-        break;
-    case CHECKOUT_TREE:
-        /* :TODO:FIX: */
-        break;
-    case CHECKOUT_HEAD:
-        /* :TODO:FIX: */
-        break;
-    default:
-        break;
-    }
-
-cleanup:
-    if (repository)
-        git_repository_free(repository);
-
-    if (err < 0) {
-        const git_error *e = giterr_last();
-        error("Error %d: %s\n", e->klass, e->message);
-    }
-
-    return R_NilValue;
 }
 
 /**
