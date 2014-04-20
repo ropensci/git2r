95464676193c1cfe6aae5463a8e0021e3a844eb0
Stefan Widgren <stefan.widgren@gmail.com>
2014-04-20 10:45:14 +0200

    Added blob test case
    * tests/blob.R: Added test case to lookup blob.

---
+++ tests/blob.R
@@ -0,0 +1,51 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+library(git2r)
+
+##
+## Create a directory in tempdir
+##
+path <- tempfile(pattern="git2r-")
+dir.create(path)
+
+##
+## Initialize a repository
+##
+repo <- init(path)
+config(repo, user.name="Repo", user.email="repo@example.org")
+
+##
+## Create a file
+##
+writeLines("Hello world!", file.path(path, "test.txt"))
+
+##
+## add and commit
+##
+add(repo, "test.txt")
+new_commit <- commit(repo, "Commit message")
+
+##
+## Lookup blob
+##
+blob <- lookup(repo, "cd0875583aabe89ee197ea133980a9085d08e497")
+stopifnot(is(blob, "git_blob"))
+
+##
+## Cleanup
+##
+unlink(path, recursive=TRUE)
