ae87c1b763da703cff4b898958358fba611ad3d3
Stefan Widgren <stefan.widgren@gmail.com>
2014-05-03 17:45:39 +0200

    Fix init slot stasher of S4 class git_stash
    * src/git2r_stash.c (stashes_cb): Fix to init slot stasher. Some minor
      code cleanup.

--- src/git2r_stash.c
+++ src/git2r_stash.c
@@ -27,6 +27,7 @@ typedef struct {
     size_t n;
     SEXP list;
     SEXP repo;
+    git_repository *repository;
 } stashes_cb_data;
 
 /**
@@ -44,12 +45,19 @@ static int stashes_cb(
     const git_oid *stash_id,
     void *payload)
 {
+    int err = 0;
     char hex[GIT_OID_HEXSZ + 1];
     SEXP stash;
+    SEXP stasher;
+    git_commit *commit = NULL;
     stashes_cb_data *cb_data = (stashes_cb_data*)payload;
 
     /* Check if we have a list to populate */
     if (R_NilValue != cb_data->list) {
+        err = git_commit_lookup(&commit, cb_data->repository, stash_id);
+        if (err < 0)
+            return err;
+
         PROTECT(stash = NEW_OBJECT(MAKE_CLASS("git_stash")));
 
         git_oid_fmt(hex, stash_id);
@@ -62,6 +70,12 @@ static int stashes_cb(
                  Rf_install("message"),
                  ScalarString(mkChar(message)));
         
+        PROTECT(stasher = NEW_OBJECT(MAKE_CLASS("git_signature")));
+        init_signature(git_commit_committer(commit), stasher);
+        SET_SLOT(stash, Rf_install("stasher"), stasher);
+        UNPROTECT(1);
+        git_commit_free(commit);
+
         SET_SLOT(stash, Rf_install("repo"), duplicate(cb_data->repo));
 
         SET_VECTOR_ELT(cb_data->list, cb_data->n, stash);
@@ -84,7 +98,7 @@ SEXP stashes(SEXP repo)
     SEXP list = R_NilValue;
     int err = 0;
     const char* err_msg = NULL;
-    stashes_cb_data cb_data = {0, R_NilValue, R_NilValue};
+    stashes_cb_data cb_data = {0, R_NilValue, R_NilValue, NULL};
     git_repository *repository = NULL;
 
     repository = get_repository(repo);
@@ -100,6 +114,7 @@ SEXP stashes(SEXP repo)
     cb_data.n = 0;
     cb_data.list = list;
     cb_data.repo = repo;
+    cb_data.repository = repository;
     err = git_stash_foreach(repository, &stashes_cb, &cb_data);
 
 cleanup:
@@ -109,12 +124,8 @@ cleanup:
     if (R_NilValue != list)
         UNPROTECT(1);
 
-    if (err < 0) {
-        if (err_msg)
-            error(err_msg);
-        else
-            error("Error: %s\n", giterr_last()->message);
-    }
+    if (err < 0)
+        error("Error: %s\n", giterr_last()->message);
 
     return list;
 }
