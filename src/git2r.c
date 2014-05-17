70d71c8d0317856439f8b4119517e2d9fc01752d
Stefan Widgren <stefan.widgren@gmail.com>
2014-05-17 08:12:34 +0200

    Fix tree pointed to by a commit or stash
    * R/tree.r (tree): Added prefix git2r_ in .Call to function
      git2r_commit_tree
    
    * src/git2r.c: Added missing git2r_commit_tree to callMethods

--- src/git2r.c
+++ src/git2r.c
@@ -579,6 +579,7 @@ static const R_CallMethodDef callMethods[] =
     {"git2r_checkout_tree", (DL_FUNC)&git2r_checkout_tree, 1},
     {"git2r_clone", (DL_FUNC)&git2r_clone, 2},
     {"git2r_commit", (DL_FUNC)&git2r_commit, 5},
+    {"git2r_commit_tree", (DL_FUNC)&git2r_commit_tree, 1},
     {"git2r_descendant_of", (DL_FUNC)&git2r_descendant_of, 2},
     {"default_signature", (DL_FUNC)&default_signature, 1},
     {"drop_stash", (DL_FUNC)&drop_stash, 2},
