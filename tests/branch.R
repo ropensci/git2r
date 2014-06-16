67e2693bf0ddbd9f4915a83a356856e15e789b1d
Stefan Widgren <stefan.widgren@gmail.com>
2014-06-16 09:48:13 +0200

    Add more branch test cases
    * tests/branch.R: Add test cases to create and delete a branch

--- tests/branch.R
+++ tests/branch.R
@@ -37,7 +37,7 @@ writeLines("Hello world!", file.path(path, "test.txt"))
 ## add and commit
 ##
 add(repo, "test.txt")
-new_commit <- commit(repo, "Commit message")
+commit.1 <- commit(repo, "Commit message")
 
 ##
 ## Check branch
@@ -47,6 +47,47 @@ stopifnot(identical(is_head(branches(repo)[[1]]), TRUE))
 stopifnot(identical(is_local(branches(repo)[[1]]), TRUE))
 stopifnot(identical(branches(repo)[[1]]@name, "master"))
 stopifnot(identical(branches(repo)[[1]], head(repo)))
+
+##
+## Create a branch
+##
+b <- branch_create(commit.1, "test")
+stopifnot(identical(b@name, "test"))
+stopifnot(identical(b@type, 1L))
+stopifnot(identical(length(branches(repo)), 2L))
+stopifnot(identical(branch_target(branches(repo)[[1]]),
+                    branch_target(branches(repo)[[2]])))
+
+##
+## Add one more commit
+##
+writeLines(c("Hello world!", "HELLO WORLD!"), file.path(path, "test.txt"))
+add(repo, "test.txt")
+commit.2 <- commit(repo, "Another commit message")
+
+##
+## Now the first branch should have moved on
+##
+stopifnot(!identical(branch_target(branches(repo)[[1]]),
+                     branch_target(branches(repo)[[2]])))
+
+##
+## Create a branch with the same name should fail
+##
+tools::assertError(branch_create(commit.2, "test"))
+
+##
+## Force it and check the branches are identical again
+##
+b <- branch_create(commit.2, "test", force=TRUE)
+stopifnot(identical(branch_target(branches(repo)[[1]]),
+                    branch_target(branches(repo)[[2]])))
+
+##
+## Delete branch
+##
+branch_delete(b)
+stopifnot(identical(length(branches(repo)), 1L))
 
 ##
 ## Cleanup
