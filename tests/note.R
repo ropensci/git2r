bb1c1c7e81141c7a0b023317c18d974fd4cfecc5
Stefan Widgren <stefan.widgren@gmail.com>
2014-06-21 21:43:35 +0200

    Add test case check notes default ref

--- tests/note.R
+++ tests/note.R
@@ -40,6 +40,11 @@ add(repo, "test.txt")
 commit.1 <- commit(repo, "Commit message")
 
 ##
+## Check default ref
+##
+stopifnot(identical(note_default_ref(repo),
+                    "refs/notes/commits"))
+##
 ## Check that note_list is an empty list
 ##
 stopifnot(identical(note_list(repo), list()))
