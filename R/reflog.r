dc044e608a8b0a2ca2f1f65515871b7bb9d7d5cf
Stefan Widgren <stefan.widgren@gmail.com>
2014-06-26 21:59:27 +0200

    Add S4 class git_reflog_entry

--- R/reflog.r
+++ R/reflog.r
@@ -13,3 +13,39 @@
 ## You should have received a copy of the GNU General Public License along
 ## with this program; if not, write to the Free Software Foundation, Inc.,
 ## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+##' Class \code{"git_reflog_entry"}
+##'
+##' @title S4 class to handle a git reflog entry.
+##' @section Slots:
+##' \describe{
+##'   \item{hex}{
+##'     40 char hexadecimal string
+##'   }
+##'   \item{message}{
+##'     The log message of the entry
+##'   }
+##'   \item{committer}{
+##'     The committer signature
+##'   }
+##'   \item{refname}{
+##'     Name of the reference
+##'   }
+##'   \item{repo}{
+##'     The S4 class git_repository that contains the reflog entry
+##'   }
+##' }
+##' @name git_reflog_entry-class
+##' @docType class
+##' @keywords classes
+##' @keywords methods
+##' @include repository.r
+##' @include signature.r
+##' @export
+setClass("git_reflog_entry",
+         slots=c(hex       = "character",
+                 message   = "character",
+                 index     = "integer",
+                 committer = "git_signature",
+                 refname   = "character",
+                 repo      = "git_repository"))
