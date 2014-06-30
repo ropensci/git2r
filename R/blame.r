687c40d7689c0d540f2ac21b405b838c9def42ef
Stefan Widgren <stefan.widgren@gmail.com>
2014-06-30 16:44:25 +0200

    Add S4 class git_blame_hunk

--- R/blame.r
+++ R/blame.r
@@ -52,3 +52,65 @@ setClass("git_blame",
              if (length(errors) == 0) TRUE else errors
          }
 )
+
+##' Class \code{"git_blame_hunk"}
+##'
+##' @title  S4 class to handle a blame hunk
+##' @section Slots:
+##' \describe{
+##'   \item{lines_in_hunk}{
+##'     The number of lines in this hunk
+##'   }
+##'   \item{final_commit_id}{
+##'     The hex of the commit where this line was last changed
+##'   }
+##'   \item{final_start_line_number}{
+##'     The 1-based line number where this hunk begins, in the final
+##'     version of the file
+##'   }
+##'   \item{final_signature}{
+##'     :TODO:DOCUMENTATION:
+##'   }
+##'   \item{orig_commit_id}{
+##'     :TODO:DOCUMENTATION:
+##'   }
+##'   \item{orig_start_line_number}{
+##'     :TODO:DOCUMENTATION:
+##'   }
+##'   \item{orig_signature}{
+##'     :TODO:DOCUMENTATION:
+##'   }
+##'   \item{orig_path}{
+##'     :TODO:DOCUMENTATION:
+##'   }
+##'   \item{boundary}{
+##'     :TODO:DOCUMENTATION:
+##'   }
+##'   \item{repo}{
+##'     The S4 class git_repository that contains the blame hunk
+##'   }
+##' }
+##' @rdname git_blame_hunk-class
+##' @docType class
+##' @keywords classes
+##' @keywords methods
+##' @include repository.r
+##' @include signature.r
+##' @export
+setClass("git_blame_hunk",
+         slots=c(lines_in_hunk           = "integer",
+                 final_commit_id         = "character",
+                 final_start_line_number = "integer",
+                 final_signature         = "git_signature",
+                 orig_commit_id          = "character",
+                 orig_start_line_number  = "integer",
+                 orig_signature          = "git_signature",
+                 orig_path               = "character",
+                 boundary                = "logical",
+                 repo                    = "git_repository"),
+         validity=function(object) {
+             errors <- character()
+
+             if (length(errors) == 0) TRUE else errors
+         }
+)
