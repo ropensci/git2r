1b32562ef47f68ba78cac7cf363613b5fed25570
Stefan Widgren <stefan.widgren@gmail.com>
2014-06-30 16:38:31 +0200

    Add S4 class git_blame

--- R/blame.r
+++ R/blame.r
@@ -13,3 +13,42 @@
 ## You should have received a copy of the GNU General Public License along
 ## with this program; if not, write to the Free Software Foundation, Inc.,
 ## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+##' Class \code{"git_blame"}
+##'
+##' @title  S4 class to handle a git blame for a single file
+##' @section Slots:
+##' \describe{
+##'   \item{path}{
+##'     The path to the file of the blame
+##'   }
+##'   \item{hunks}{
+##'     List of blame hunks
+##'   }
+##'   \item{repo}{
+##'     The S4 class git_repository that contains the file
+##'   }
+##' }
+##' @rdname git_blame-class
+##' @docType class
+##' @keywords classes
+##' @keywords methods
+##' @include repository.r
+##' @export
+##' @examples \dontrun{
+##' ## Open an existing repository
+##' repo <- repository("path/to/git2r")
+##'
+##' ## Get blame for file in repository
+##' blame(repo, ".gitignore")
+##' }
+setClass("git_blame",
+         slots=c(path  = "character",
+                 hunks = "list",
+                 repo  = "git_repository"),
+         validity=function(object) {
+             errors <- character()
+
+             if (length(errors) == 0) TRUE else errors
+         }
+)
