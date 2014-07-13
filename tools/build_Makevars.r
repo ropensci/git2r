5e1c06046c221de643f333ac99df3e0f713defc0
Stefan Widgren <stefan.widgren@gmail.com>
2014-07-13 16:35:10 +0200

    Add check for 'git2r_' prefix in build Makevars script
    * tools/build_Makevars.r: Raise an error if missing 'git2r_' prefix in
      git2r C functions.

--- tools/build_Makevars.r
+++ tools/build_Makevars.r
@@ -1,3 +1,24 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+##' Generate object files in path
+##'
+##' @param path The path to directory to generate object files from
+##' @param exclude Files to exclude
+##' @return Character vector with object files
 o_files <- function(path, exclude = NULL) {
     files <- sub("c$", "o",
                  sub("src/", "",
@@ -8,6 +29,11 @@ o_files <- function(path, exclude = NULL) {
     files
 }
 
+##' Generate build objects
+##'
+##' @param files The object files
+##' @param Makevars The Makevars file
+##' @return invisible NULL
 build_objects <- function(files, Makevars) {
     lapply(names(files), function(obj) {
         cat("OBJECTS.", obj, " =", sep="", file = Makevars)
@@ -32,6 +58,9 @@ build_objects <- function(files, Makevars) {
     invisible(NULL)
 }
 
+##' Build Makevars.in
+##'
+##' @return invisible NULL
 build_Makevars.in <- function() {
     Makevars <- file("src/Makevars.in", "w")
     on.exit(close(Makevars))
@@ -51,8 +80,13 @@ build_Makevars.in <- function() {
     cat("\n", file = Makevars)
 
     build_objects(files, Makevars)
+
+    invisible(NULL)
 }
 
+##' Build Makevars.in
+##'
+##' @return invisible NULL
 build_Makevars.win <- function() {
     Makevars <- file("src/Makevars.win", "w")
     on.exit(close(Makevars))
@@ -72,7 +106,85 @@ build_Makevars.win <- function() {
     cat("\n", file = Makevars)
 
     build_objects(files, Makevars)
+
+    invisible(NULL)
 }
 
+##' Extract .NAME in .Call(.NAME
+##'
+##' @param files R files to extract .NAME from
+##' @return data.frame with columns filename and .NAME
+extract_git2r_calls <- function(files) {
+    df <- lapply(files, function(filename) {
+        ## Read file
+        lines <- readLines(file.path("R", filename))
+
+        ## Trim comments
+        comments <- gregexpr("#", lines)
+        for (i in seq_len(length(comments))) {
+            start <- as.integer(comments[[i]])
+            if (start[1] > 0) {
+                if (start[1] > 1) {
+                    lines[i] <- substr(lines[i], 1, start[1])
+                } else {
+                    lines[i] <- ""
+                }
+            }
+        }
+
+        ## Trim whitespace
+        lines <- sub("^\\s*", "", sub("\\s*$", "", lines))
+
+        ## Collapse to one line
+        lines <- paste0(lines, collapse=" ")
+
+        ## Find .Call
+        pattern <- "[.]Call[[:space:]]*[(][[:space:]]*[.[:alpha:]\"][^\",]*"
+        calls <- gregexpr(pattern, lines)
+        start <- as.integer(calls[[1]])
+
+        if(start[1] > 0) {
+            ## Extract .Call
+            len <- attr(calls[[1]], "match.length")
+            calls <- substr(rep(lines, length(start)), start, start + len - 1)
+
+            ## Trim .Call to extract .NAME
+            pattern <- "[.]Call[[:space:]]*[(][[:space:]]*[\"]?"
+            calls <- sub(pattern, "", calls)
+            return(data.frame(filename = filename,
+                              .NAME = calls,
+                              stringsAsFactors = FALSE))
+        }
+
+        return(NULL)
+    })
+
+    df <- do.call("rbind", df)
+    df[order(df$filename),]
+}
+
+##' Check that .NAME in .Call(.NAME is prefixed with 'git2r_'
+##'
+##' Raise an error in case of missing 'git2r_' prefix
+##' @param calls data.frame with the name of the C function to call
+##' @return invisible NULL
+check_git2r_prefix <- function(calls) {
+    .NAME <- grep("git2r_", calls$.NAME, value=TRUE, invert=TRUE)
+
+    if (!identical(length(.NAME), 0L)) {
+        i <- which(calls$.NAME == .NAME)
+        msg <- sprintf("%s in %s\n", calls$.NAME[i], calls$filename[i])
+        msg <- c("\n\nMissing 'git2r_' prefix:\n", msg, "\n")
+        stop(msg)
+    }
+
+    invisible(NULL)
+}
+
+## Check that all git2r C functions are prefixed with 'git2r_'
+calls <- extract_git2r_calls(list.files("R", "*.r"))
+check_git2r_prefix(calls)
+
+## Generate Makevars
 build_Makevars.in()
 build_Makevars.win()
