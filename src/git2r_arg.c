ac8fb14704e7b2f8fd0dd91fc3f0e2b7df559c5c
Stefan Widgren <stefan.widgren@gmail.com>
2014-08-29 23:42:25 +0200

    Return GIT_OK or GIT_ERROR from git2r C code argument checks
    * src/git2r_arg.c: Use GIT_OK and GIT_ERROR instead of 0 and -1 to
      improve readability.
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- src/git2r_arg.c
+++ src/git2r_arg.c
@@ -25,30 +25,30 @@
  * Check blob argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_blob(SEXP arg)
 {
     SEXP class_name;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_blob"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check branch argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_branch(SEXP arg)
 {
@@ -56,57 +56,57 @@ int git2r_arg_check_branch(SEXP arg)
     SEXP slot;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_branch"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("name"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("name"))))
+        return GIT_ERROR;
 
     slot = GET_SLOT(arg, Rf_install("type"));
-    if (0 != git2r_arg_check_integer(slot))
-        return -1;
+    if (GIT_OK != git2r_arg_check_integer(slot))
+        return GIT_ERROR;
     switch (INTEGER(slot)[0]) {
     case GIT_BRANCH_LOCAL:
     case GIT_BRANCH_REMOTE:
         break;
     default:
-        return -1;
+        return GIT_ERROR;
     }
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check commit argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_commit(SEXP arg)
 {
     SEXP class_name;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_commit"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check credentials argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_credentials(SEXP arg)
 {
@@ -114,31 +114,31 @@ int git2r_arg_check_credentials(SEXP arg)
 
     /* It's ok if the credentials is R_NilValue */
     if (R_NilValue == arg)
-        return 0;
+        return GIT_OK;
 
     if (S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 == strcmp(CHAR(STRING_ELT(class_name, 0)), "cred_user_pass")) {
         /* Check username and password */
-        if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("username"))))
-            return -1;
-        if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("password"))))
-            return -1;
+        if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("username"))))
+            return GIT_ERROR;
+        if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("password"))))
+            return GIT_ERROR;
     } else if (0 == strcmp(CHAR(STRING_ELT(class_name, 0)), "cred_ssh_key")) {
         SEXP passphrase;
 
         /* Check public and private key */
-        if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("publickey"))))
-            return -1;
-        if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("privatekey"))))
-            return -1;
+        if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("publickey"))))
+            return GIT_ERROR;
+        if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("privatekey"))))
+            return GIT_ERROR;
 
         /* Check that passphrase is a character vector */
         passphrase = GET_SLOT(arg, Rf_install("passphrase"));
-        if (0 != git2r_arg_check_string_vec(passphrase))
-            return -1;
+        if (GIT_OK != git2r_arg_check_string_vec(passphrase))
+            return GIT_ERROR;
 
         /* Check that length of passphrase < 2, i.e. it's either
          * character(0) or some "passphrase" */
@@ -147,16 +147,16 @@ int git2r_arg_check_credentials(SEXP arg)
             break;
         case 1:
             if (NA_STRING == STRING_ELT(passphrase, 0))
-                return -1;
+                return GIT_ERROR;
             break;
         default:
-            return -1;
+            return GIT_ERROR;
         }
     } else {
-        return -1;
+        return GIT_ERROR;
     }
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
@@ -167,54 +167,54 @@ int git2r_arg_check_credentials(SEXP arg)
  *  - Zero length character vector
  *  - character vector of length one with strlen(value) > 0
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_filename(SEXP arg)
 {
     if (R_NilValue == arg)
-        return 0;
+        return GIT_OK;
     if (!isString(arg))
-        return -1;
+        return GIT_ERROR;
     switch (length(arg)) {
     case 0:
         break;
     case 1:
         if (NA_STRING == STRING_ELT(arg, 0))
-            return -1;
+            return GIT_ERROR;
         if (0 == strlen(CHAR(STRING_ELT(arg, 0))))
-            return -1;
+            return GIT_ERROR;
     default:
-        return -1;
+        return GIT_ERROR;
     }
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check hex argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_hex(SEXP arg)
 {
     size_t len;
 
-    if (0 != git2r_arg_check_string(arg))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(arg))
+        return GIT_ERROR;
 
     len = LENGTH(STRING_ELT(arg, 0));
     if (len < GIT_OID_MINPREFIXLEN || len > GIT_OID_HEXSZ)
-        return -1;
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check integer argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_integer(SEXP arg)
 {
@@ -222,36 +222,36 @@ int git2r_arg_check_integer(SEXP arg)
         || !isInteger(arg)
         || 1 != length(arg)
         || NA_INTEGER == INTEGER(arg)[0])
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 /**
  * Check integer argument and that arg is greater than or equal to 0.
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_integer_gte_zero(SEXP arg)
 {
-    if (0 != git2r_arg_check_integer(arg))
-        return -1;
+    if (GIT_OK != git2r_arg_check_integer(arg))
+        return GIT_ERROR;
     if (0 > INTEGER(arg)[0])
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 /**
  * Check list argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_list(SEXP arg)
 {
     if (R_NilValue == arg || !isNewList(arg))
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 
@@ -259,7 +259,7 @@ int git2r_arg_check_list(SEXP arg)
  * Check logical argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_logical(SEXP arg)
 {
@@ -267,41 +267,41 @@ int git2r_arg_check_logical(SEXP arg)
         || !isLogical(arg)
         || 1 != length(arg)
         || NA_LOGICAL == LOGICAL(arg)[0])
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 /**
  * Check note argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_note(SEXP arg)
 {
     SEXP class_name;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_note"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("refname"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("refname"))))
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check real argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_real(SEXP arg)
 {
@@ -309,15 +309,15 @@ int git2r_arg_check_real(SEXP arg)
         || !isReal(arg)
         || 1 != length(arg)
         || NA_REAL == REAL(arg)[0])
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 /**
  * Check signature argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_signature(SEXP arg)
 {
@@ -325,24 +325,24 @@ int git2r_arg_check_signature(SEXP arg)
     SEXP when;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_signature"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("name"))))
-        return -1;
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("email"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("name"))))
+        return GIT_ERROR;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("email"))))
+        return GIT_ERROR;
 
     when = GET_SLOT(arg, Rf_install("when"));
-    if (0 != git2r_arg_check_real(GET_SLOT(when, Rf_install("time"))))
-        return -1;
-    if (0 != git2r_arg_check_real(GET_SLOT(when, Rf_install("offset"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_real(GET_SLOT(when, Rf_install("time"))))
+        return GIT_ERROR;
+    if (GIT_OK != git2r_arg_check_real(GET_SLOT(when, Rf_install("offset"))))
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
@@ -351,15 +351,15 @@ int git2r_arg_check_signature(SEXP arg)
  * Compared to git2r_arg_check_string_vec, also checks that length of vector
  * is one and non-NA.
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_string(SEXP arg)
 {
     if (git2r_arg_check_string_vec(arg) < 0)
-        return -1;
+        return GIT_ERROR;
     if (1 != length(arg) || NA_STRING == STRING_ELT(arg, 0))
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 /**
@@ -368,57 +368,57 @@ int git2r_arg_check_string(SEXP arg)
  * Compared to git2r_arg_check_string, only checks that argument is non-null
  * and string. Use git2r_arg_check_string to check scalar string.
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_string_vec(SEXP arg)
 {
     if (R_NilValue == arg || !isString(arg))
-        return -1;
-    return 0;
+        return GIT_ERROR;
+    return GIT_OK;
 }
 
 /**
  * Check tag argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_tag(SEXP arg)
 {
     SEXP class_name;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_tag"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("target"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("target"))))
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
 
 /**
  * Check tree argument
  *
  * @param arg the arg to check
- * @return 0 if OK, else -1
+ * @return GIT_OK if OK, else GIT_ERROR
  */
 int git2r_arg_check_tree(SEXP arg)
 {
     SEXP class_name;
 
     if (R_NilValue == arg || S4SXP != TYPEOF(arg))
-        return -1;
+        return GIT_ERROR;
 
     class_name = getAttrib(arg, R_ClassSymbol);
     if (0 != strcmp(CHAR(STRING_ELT(class_name, 0)), "git_tree"))
-        return -1;
+        return GIT_ERROR;
 
-    if (0 != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
-        return -1;
+    if (GIT_OK != git2r_arg_check_string(GET_SLOT(arg, Rf_install("hex"))))
+        return GIT_ERROR;
 
-    return 0;
+    return GIT_OK;
 }
