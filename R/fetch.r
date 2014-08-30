ea3a88905aef49e7fc84049687286a8e5b201b6d
Stefan Widgren <stefan.widgren@gmail.com>
2014-08-30 11:23:34 +0200

    Add method fetch_heads
    * R/fetch.r (fetch_heads): Get updated heads during the last fetch.
    
    * man/fetch_heads-methods.Rd: Add documentation
    
    * NAMESPACE: Update
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/fetch.r
+++ R/fetch.r
@@ -50,3 +50,60 @@ setMethod("fetch",
               invisible(result)
           }
 )
+
+##' Get updated heads during the last fetch.
+##'
+##' @rdname fetch_heads-methods
+##' @docType methods
+##' @param repo the repository
+##' @return list with the S4 class \code{\linkS4class{git_fetch_head}}
+##' entries. NULL if there is no FETCH_HEAD file.
+##' @keywords methods
+##' @include S4_classes.r
+##' @examples
+##' \dontrun{
+##' ## Initialize three temporary repositories
+##' path_bare <- tempfile(pattern="git2r-")
+##' path_repo_1 <- tempfile(pattern="git2r-")
+##' path_repo_2 <- tempfile(pattern="git2r-")
+##'
+##' dir.create(path_bare)
+##' dir.create(path_repo_1)
+##' dir.create(path_repo_2)
+##'
+##' bare_repo <- init(path_bare, bare = TRUE)
+##' repo_1 <- clone(path_bare, path_repo_1)
+##' repo_2 <- clone(path_bare, path_repo_2)
+##'
+##' config(repo_1, user.name="Repo One", user.email="repo.one@@example.org")
+##' config(repo_2, user.name="Repo Two", user.email="repo.two@@example.org")
+##'
+##' ## Add changes to repo 1
+##' writeLines("Lorem ipsum dolor sit amet",
+##'            con = file.path(path_repo_1, "example.txt"))
+##' add(repo_1, "example.txt")
+##' commit(repo_1, "Commit message")
+##'
+##' ## Push changes from repo 1 to origin (bare_repo)
+##' push(repo_1, "origin", "refs/heads/master")
+##'
+##' ## Fetch changes from origin (bare_repo) to repo 2
+##' fetch(repo_2, "origin")
+##'
+##' ## List updated heads
+##' fetch_heads(repo_2)
+##' }
+setGeneric("fetch_heads",
+           signature = "repo",
+           function(repo)
+           standardGeneric("fetch_heads"))
+
+##' @rdname fetch_heads-methods
+##' @export
+setMethod("fetch_heads",
+          signature(repo = "git_repository"),
+          function (repo)
+          {
+              .Call(git2r_repository_fetch_heads, repo)
+          }
+)
