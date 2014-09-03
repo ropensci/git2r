18713eb73c965da8d1c48acf11ec9bfb3f268206
Stefan Widgren <stefan.widgren@gmail.com>
2014-09-03 22:53:16 +0200

    Add internal function to perform a fast-forward merge
    * src/git2r_merge.c (git2r_fast_forward_merge): Add function to
      perform a fast-forward merge.
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- src/git2r_merge.c
+++ src/git2r_merge.c
@@ -94,3 +94,88 @@ cleanup:
 
     return result;
 }
+
+/**
+ * Perform a fast-forward merge
+ *
+ * @param merge_head The merge head to fast-forward merge
+ * @param repository The repository
+ * @param name The name of the merge in the reflog
+ * @param merger Who is performing the merge
+ * @return 0 on success, or error code
+ */
+static int git2r_fast_forward_merge(
+    const git_merge_head *merge_head,
+    git_repository *repository,
+    const char *name,
+    git_signature *merger)
+{
+    int err;
+    const git_oid *oid;
+    git_buf buf = GIT_BUF_INIT;
+    git_commit *commit = NULL;
+    git_tree *tree = NULL;
+    git_reference *reference = NULL;
+    git_checkout_options opts = GIT_CHECKOUT_OPTIONS_INIT;
+
+    oid = git_merge_head_id(merge_head);
+    err = git_commit_lookup(&commit, repository, oid);
+    if (GIT_OK != err)
+        goto cleanup;
+
+    err = git_commit_tree(&tree, commit);
+    if (GIT_OK != err)
+        goto cleanup;
+
+    opts.checkout_strategy = GIT_CHECKOUT_SAFE;
+    err = git_checkout_tree(repository, (git_object*)tree, &opts);
+    if (GIT_OK != err)
+        goto cleanup;
+
+    err = git_repository_head(&reference, repository);
+    if (GIT_OK != err) {
+        if (GIT_ENOTFOUND != err)
+            goto cleanup;
+    }
+
+    err = git_buf_printf(&buf, "merge %s: Fast-forward", name);
+    if (GIT_OK != err)
+        goto cleanup;
+
+    if (GIT_ENOTFOUND == err) {
+        err = git_reference_create(
+            &reference,
+            repository,
+            "HEAD",
+            git_commit_id(commit),
+            0, /* force */
+            merger,
+            buf.ptr);
+    } else {
+        git_reference *target_ref = NULL;
+
+        err = git_reference_set_target(
+            &target_ref,
+            reference,
+            git_commit_id(commit),
+            merger,
+            buf.ptr);
+
+        if (target_ref)
+            git_reference_free(target_ref);
+    }
+
+    git_buf_free(&buf);
+
+cleanup:
+    if (commit)
+        git_commit_free(commit);
+
+    if (reference)
+        git_reference_free(reference);
+
+    if (tree)
+        git_tree_free(tree);
+
+    return err;
+}
