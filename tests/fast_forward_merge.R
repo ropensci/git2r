f7aa15cd1cfba32a6774b40cbe57d7c78a97f42f
Stefan Widgren <stefan.widgren@gmail.com>
2014-09-06 12:48:48 +0200

    Add fast-forward merge test case
    * tests/fast_forward_merge.R: Add test case
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

---
+++ tests/fast_forward_merge.R
@@ -0,0 +1,59 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+library(git2r)
+
+## Create directory for repository in tempdir
+path <- tempfile(pattern="git2r-")
+dir.create(path)
+
+## Create repository
+repo <- init(path)
+config(repo, user.name="Repo", user.email="repo@example.org")
+
+## Add changes to repo
+writeLines("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+           con = file.path(path, "test-1.txt"))
+add(repo, "test-1.txt")
+commit_1 <- commit(repo, "First commit message")
+
+## Create branch and checkout
+checkout(branch_create(commit_1, name = "test"))
+
+## Add changes to test branch
+writeLines(c("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+             "eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+           con = file.path(path, "test-1.txt"))
+add(repo, "test-1.txt")
+commit_2 <- commit(repo, "Second commit message")
+
+# Checkout master and merge
+b <- branches(repo)
+checkout(b[sapply(b, slot, "name") == "master"][[1]], force=TRUE)
+m <- merge(b[sapply(b, slot, "name") == "test"][[1]])
+
+# Check merge
+stopifnot(is(object = m, class2 = "git_merge_result"))
+stopifnot(identical(m@status, "Fast-forward"))
+stopifnot(identical(m@conflicts, FALSE))
+stopifnot(identical(length(commits(repo)), 2L))
+
+# Check reflog
+r <- reflog(repo)
+stopifnot(identical(r[[1]]@message, "merge test: Fast-forward"))
+
+## Cleanup
+unlink(path, recursive=TRUE)
