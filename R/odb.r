67a12a1c723a3c35be6c0a44048854b0ced65b6c
Stefan Widgren <stefan.widgren@gmail.com>
2014-09-20 10:23:59 +0200

    List blobs in the object database
    * R/odb.r (odb_blobs): List all blobs reachable from the commits in
      the object database. For each commit, list blob's in the commit tree
      and sub-tree's. Return a data.frame with the following columns:
      - sha: The sha of the blob.
      - path: The path to the blob from the tree and sub-trees.
      - name: The name of the blob from the tree that contains the blob.
      - len: The length of the blob.
      - commit: The sha of the commit.
      - author: The author of the commit.
      - when: The timestamp of the author signature in the commit.
    
    * Note: A blob sha can have several entries
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/odb.r
+++ R/odb.r
@@ -14,6 +14,79 @@
 ## with this program; if not, write to the Free Software Foundation, Inc.,
 ## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+##' Blobs in the object database
+##'
+##' List all blobs reachable from the commits in the object
+##' database. For each commit, list blob's in the commit tree and
+##' sub-trees.
+##' @rdname odb_blobs-methods
+##' @docType methods
+##' @param repo The repository
+##' @return A data.frame with the following columns:
+##' \describe{
+##'   \item{sha}{The sha of the blob}
+##'   \item{path}{The path to the blob from the tree and sub-trees}
+##'   \item{name}{The name of the blob from the tree that contains the blob}
+##'   \item{len}{The length of the blob}
+##'   \item{commit}{The sha of the commit}
+##'   \item{author}{The author of the commit}
+##'   \item{when}{The timestamp of the author signature in the commit}
+##' }
+##' @note A blob sha can have several entries
+##' @keywords methods
+##' @examples \dontrun{
+##' ## Create a directory in tempdir
+##' path <- tempfile(pattern="git2r-")
+##' dir.create(path)
+##'
+##' ## Initialize a repository
+##' repo <- init(path)
+##' config(repo, user.name="Developer", user.email="developer@@example.org")
+##'
+##' ## Create a file, add and commit
+##' writeLines("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+##'            con = file.path(path, "test.txt"))
+##' add(repo, "test.txt")
+##' commit(repo, "Commit message 1")
+##'
+##' ## Change file and commit
+##' writeLines(c("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+##'              "eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+##'              con = file.path(path, "test.txt"))
+##' add(repo, "test.txt")
+##' commit(repo, "Commit message 2")
+##'
+##' ## Commit same content under different name in a sub-directory
+##' dir.create(file.path(path, "sub-directory"))
+##' file.copy(file.path(path, "test.txt"), file.path(path, "sub-directory", "copy.txt"))
+##' add(repo, "sub-directory/copy.txt")
+##' commit(repo, "Commit message 3")
+##'
+##' ## List blobs
+##' odb_blobs(repo)
+##' }
+setGeneric("odb_blobs",
+           signature = "repo",
+           function(repo)
+           standardGeneric("odb_blobs"))
+
+##' @rdname odb_blobs-methods
+##' @export
+setMethod("odb_blobs",
+          signature(repo = "git_repository"),
+          function(repo)
+          {
+              blobs <- data.frame(.Call(git2r_odb_blobs, repo),
+                                   stringsAsFactors = FALSE)
+              blobs <- blobs[order(blobs$when),]
+              index <- paste0(blobs$sha, ":", blobs$path, "/", blobs$name)
+              blobs <- blobs[!duplicated(index),]
+              rownames(blobs) <- NULL
+              blobs$when <- as.POSIXct(blobs$when, origin="1970-01-01", tz="GMT")
+              blobs
+          }
+)
+
 ##' List all objects available in the database
 ##'
 ##' @rdname odb_list-methods
