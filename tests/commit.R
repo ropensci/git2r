973024466978eeff0936eab383d3dd985de060c6
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-01 09:05:31 +0200

    Add test case for contributions method
    * tests/commit.R: Add test case for contributions method. Calls C
      function git2r_revwalk_contributions.
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- tests/commit.R
+++ tests/commit.R
@@ -16,42 +16,28 @@
 
 library(git2r)
 
-##
 ## Create a directory in tempdir
-##
 path <- tempfile(pattern="git2r-")
 dir.create(path)
 
-##
 ## Initialize a repository
-##
 repo <- init(path)
 config(repo, user.name="Repo", user.email="repo@example.org")
 
-##
 ## Commit without adding changes should produce an error
-##
 tools::assertError(commit(repo, "Test to commit"))
 
-##
 ## Create a file
-##
 writeLines("Hello world!", file.path(path, "test.txt"))
 
-##
 ## Commit without adding changes should produce an error
-##
 tools::assertError(commit(repo, "Test to commit"))
 
-##
-## add and commit
-##
+## Add and commit
 add(repo, "test.txt")
 commit_1 <- commit(repo, "Commit message")
 
-##
 ## Check commit
-##
 stopifnot(identical(commit_1@author@name, "Repo"))
 stopifnot(identical(commit_1@author@email, "repo@example.org"))
 stopifnot(identical(lookup(repo, commit_1@sha), commit_1))
@@ -60,27 +46,28 @@ stopifnot(identical(commits(repo)[[1]]@author@name, "Repo"))
 stopifnot(identical(commits(repo)[[1]]@author@email, "repo@example.org"))
 stopifnot(identical(parents(commit_1), list()))
 
-##
 ## Commit without adding changes should produce an error
-##
 tools::assertError(commit(repo, "Test to commit"))
 
-##
 ## Add another commit
-##
 writeLines(c("Hello world!", "HELLO WORLD!"), file.path(path, "test.txt"))
 add(repo, "test.txt")
 commit_2 <- commit(repo, "Commit message 2")
 
-##
 ## Check relationship
-##
 stopifnot(identical(descendant_of(commit_2, commit_1), TRUE))
 stopifnot(identical(descendant_of(commit_1, commit_2), FALSE))
 stopifnot(identical(length(parents(commit_2)), 1L))
 stopifnot(identical(parents(commit_2)[[1]], commit_1))
 
-##
+## Check contributions
+stopifnot(identical(colnames(contributions(repo, by="author", breaks="day")),
+                    c("when", "author", "n")))
+stopifnot(identical(colnames(contributions(repo)),
+                    c("when", "n")))
+stopifnot(identical(nrow(contributions(repo)), 1L))
+stopifnot(identical(contributions(repo)$n, 2L))
+stopifnot(identical(contributions(repo, by="author", breaks="day")$n, 2L))
+
 ## Cleanup
-##
 unlink(path, recursive=TRUE)
