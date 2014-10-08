aa531b1284bba9ba27a90332f873af519ee3b45a
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-08 18:25:37 +0200

    Add test for odb_objects method
    * tests/tag.R: Add test case for odb_objects method. Calls C function
      git2r_odb_objects.
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- tests/tag.R
+++ tests/tag.R
@@ -16,10 +16,8 @@
 
 library(git2r)
 
-##
 ## Check validity of S4 class git_tag
 ## Each slot must have length equal to one
-##
 when <- new("git_time", time = 1395567947, offset = 60)
 tagger <- new("git_signature",
               name = "Repo",
@@ -62,46 +60,28 @@ tools::assertError(validObject(new("git_tag",
                                    tagger = tagger,
                                    target = c("target1", "target2"))))
 
-##
-## Create tag in repository...
-##
-
-##
 ## Create a directory in tempdir
-##
 path <- tempfile(pattern="git2r-")
 dir.create(path)
 
-##
 ## Initialize a repository
-##
 repo <- init(path)
 config(repo, user.name="Repo", user.email="repo@example.org")
 
-##
 ## Create a file
-##
 writeLines("Hello world!", file.path(path, "test.txt"))
 
-##
 ## add and commit
-##
 add(repo, 'test.txt')
 commit(repo, "Commit message")
 
-##
 ## Check tags, no tag added
-##
 stopifnot(identical(tags(repo), list()))
 
-##
 ## Create tag
-##
 new_tag <- tag(repo, "Tagname", "Tag message")
 
-##
 ## Check tag
-##
 stopifnot(identical(lookup(repo, new_tag@sha), new_tag))
 stopifnot(identical(new_tag@name, "Tagname"))
 stopifnot(identical(new_tag@message, "Tag message"))
@@ -113,7 +93,14 @@ stopifnot(identical(tags(repo)[[1]]@message, "Tag message"))
 stopifnot(identical(tags(repo)[[1]]@tagger@name, "Repo"))
 stopifnot(identical(tags(repo)[[1]]@tagger@email, "repo@example.org"))
 
-##
+## Check objects in object database
+stopifnot(identical(table(odb_objects(repo)$type),
+                    structure(c(1L, 1L, 1L, 1L),
+                              .Dim = 4L,
+                              .Dimnames = structure(list(
+                                  c("blob", "commit", "tag", "tree")),
+                                  .Names = ""),
+                              class = "table")))
+
 ## Cleanup
-##
 unlink(path, recursive=TRUE)
