d3507a15d3c9bc285dae79909daf430decf1145b
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-14 19:35:15 +0200

    Add test for odb_blobs method
    * tests/odb_blobs.R: Add test case for odb_blobs method. Calls C
      function git2r_odb_blobs.
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

---
+++ tests/odb_blobs.R
@@ -0,0 +1,61 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2014 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+library(git2r)
+
+## Create a directory in tempdir
+path <- tempfile(pattern="git2r-")
+dir.create(path)
+
+## Initialize a repository
+repo <- init(path)
+config(repo, user.name="Developer", user.email="developer@@example.org")
+
+## Create a file, add and commit
+writeLines("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+           con = file.path(path, "test.txt"))
+add(repo, "test.txt")
+commit(repo, "Commit message 1")
+
+## Change file and commit
+writeLines(c("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+             "eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+             con = file.path(path, "test.txt"))
+add(repo, "test.txt")
+commit(repo, "Commit message 2")
+
+## Commit same content under different name in a sub-directory
+dir.create(file.path(path, "sub-directory"))
+file.copy(file.path(path, "test.txt"), file.path(path, "sub-directory", "copy.txt"))
+add(repo, "sub-directory/copy.txt")
+commit(repo, "Commit message 3")
+
+## List blobs
+b <- odb_blobs(repo)
+
+## Order the data.frame before checking
+b <- b[order(b$name),]
+
+## Check blobs
+stopifnot(identical(nrow(b), 3L))
+stopifnot(identical(colnames(b),
+                    c("sha", "path", "name", "len", "commit", "author", "when")))
+stopifnot(identical(b$path, c("sub-directory", "", "")))
+stopifnot(identical(b$name, c("copy.txt", "test.txt", "test.txt")))
+stopifnot(identical(b$author, c("Developer", "Developer", "Developer")))
+
+## Cleanup
+unlink(path, recursive=TRUE)
