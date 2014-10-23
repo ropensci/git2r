93c9961e0f42f273d8207a22a4b7f00552d6e789
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-23 07:43:49 +0200

    Add method to checkout named branch
    * R/checkout.r: Add checkout method for signature git_repository. The
      method takes one argument 'branch' with name of branch to checkout. If
      the branch doesn't exists, checks for an unique remote with name equal
      to branch and creates a local branch tracking remote. If no remote
      branch, creates a new branch if 'create' equals TRUE.
    
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/checkout.r
+++ R/checkout.r
@@ -42,8 +42,13 @@ checkout_reflog_msg <- function(object, ref_log_target) {
 ##' (force = FALSE) or GIT_CHECKOUT_FORCE (force = TRUE).
 ##' @rdname checkout-methods
 ##' @docType methods
-##' @param object a commit, tag or tree which content will be used to
-##' update the working directory.
+##' @param object a repository, commit, tag or tree which content will
+##' be used to update the working directory.
+##' @param ... Additional arguments affecting the checkout
+##' @param branch If object is a repository, the name of the branch to
+##' check out.
+##' @param create If object is a repository, then branch is created if
+##' doesn't exist.
 ##' @param force If TRUE, then make working directory match
 ##' target. This will throw away local changes. Default is FALSE.
 ##' @return invisible NULL
@@ -51,15 +56,63 @@ checkout_reflog_msg <- function(object, ref_log_target) {
 ##' @include S4_classes.r
 setGeneric("checkout",
            signature = "object",
-           function (object, force = FALSE)
+           function (object, ...)
            standardGeneric("checkout")
 )
 
 ##' @rdname checkout-methods
 ##' @export
 setMethod("checkout",
+          signature(object = "git_repository"),
+          function (object, branch, create = FALSE, force = FALSE)
+          {
+              ## Check branch argument
+              if (missing(branch))
+                  stop("missing 'branch' argument")
+              if (any(!is.character(branch), !identical(length(branch), 1L)))
+                  stop("'branch' must be a character vector of length one")
+
+              ## Check if branch exists in a local branch
+              lb <- branches(object, "local")
+              lb <- lb[sapply(lb, slot, "name") == branch]
+              if (length(lb)) {
+                  checkout(lb[[1]], force = force)
+              } else {
+                  ## Check if there exists exactly one remote branch
+                  ## with a matching name.
+                  rb <- branches(object, "remote")
+
+                  ## Split remote/name to check for a unique name
+                  name <- sapply(rb, function(x) {
+                      remote <- strsplit(x@name, "/")[[1]][1]
+                      sub(paste0("^", remote, "/"), "", x@name)
+                  })
+                  i <- which(name == branch)
+                  if (identical(length(i), 1L)) {
+                      ## Create branch and track remote
+                      commit <- lookup(object, branch_target(rb[[i]]))
+                      branch <- branch_create(commit, branch)
+                      branch_set_upstream(branch, rb[[1]]@name)
+                      checkout(branch, force = TRUE)
+                  } else {
+                      if (!identical(create, TRUE))
+                          stop(sprintf("'%s' did not match any branch", branch))
+
+                      ## Create branch
+                      commit <- lookup(object, branch_target(head(object)))
+                      checkout(branch_create(commit, branch), force = force)
+                  }
+              }
+
+              invisible(NULL)
+          }
+)
+
+##' @rdname checkout-methods
+##' @export
+setMethod("checkout",
           signature(object = "git_branch"),
-          function (object, force)
+          function (object, force = FALSE)
           {
               ret <- .Call(
                   git2r_checkout_branch,
@@ -75,7 +128,7 @@ setMethod("checkout",
 ##' @export
 setMethod("checkout",
           signature(object = "git_commit"),
-          function (object, force)
+          function (object, force = FALSE)
           {
               ret <- .Call(
                   git2r_checkout_commit,
@@ -91,7 +144,7 @@ setMethod("checkout",
 ##' @export
 setMethod("checkout",
           signature(object = "git_tag"),
-          function (object, force)
+          function (object, force = FALSE)
           {
               ret <- .Call(
                   git2r_checkout_tag,
