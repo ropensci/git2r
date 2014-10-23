c333ee1829eff02b2f7fe20a24d156714be7a4f7
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-23 17:27:42 +0200

    Fix R CMD check warning undocumented summary arguments
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/diff.r
+++ R/diff.r
@@ -94,6 +94,7 @@ hunks_per_file <- function(diff) {
 #' @aliases summary,git_diff-methods
 #' @docType methods
 #' @param object The diff \code{object}.
+#' @param ... Additional arguments affecting the summary produced.
 #' @keywords methods
 #' @export
 setMethod("summary",
@@ -228,7 +229,6 @@ setMethod("diff_print",
 #' @param new_tree The new git_tree object to compare, or NULL.
 #'        If NULL, then we use the working directory or the index (see
 #'        the \code{index} argument).
-#' @param ... Additional arguments affecting the summary produced.
 #' @return None (invisible 'NULL').
 #' @export
 setMethod("diff_print",
