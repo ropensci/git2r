6ce4c22f7bce2fcc22f8f4eb7a9ad59b911e11d4
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-23 18:17:19 +0200

    Add example to branches method
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/branch.r
+++ R/branch.r
@@ -280,6 +280,32 @@ setMethod("branch_set_upstream",
 ##' @return list of branches in repository
 ##' @keywords methods
 ##' @include S4_classes.r
+##' @examples
+##' \dontrun{
+##' ## Initialize repositories
+##' path_bare <- tempfile(pattern="git2r-")
+##' path_repo <- tempfile(pattern="git2r-")
+##' dir.create(path_bare)
+##' dir.create(path_repo)
+##' repo_bare <- init(path_bare, bare = TRUE)
+##' repo <- clone(path_bare, path_repo)
+##'
+##' ## Config first user and commit a file
+##' config(repo, user.name="User", user.email="user@@example.org")
+##'
+##' ## Write to a file and commit
+##' writeLines("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+##'            file.path(path_repo, "example.txt"))
+##' add(repo, "example.txt")
+##' commit(repo, "First commit message")
+##'
+##' ## Push commits from repository to bare repository
+##' ## Adds an upstream tracking branch to branch 'master'
+##' push(repo, "origin", "refs/heads/master")
+##'
+##' ## List branches
+##' branches(repo)
+##' }
 setGeneric("branches",
            signature = "repo",
            function(repo, flags=c("all", "local", "remote"))
