9bc4244214f9ca78b5e69462ef699f4e94422a42
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-26 15:59:26 +0100

    Add more diff tests
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- tests/diff.R
+++ tests/diff.R
@@ -41,6 +41,8 @@ commit(repo, "Commit message")
 writeLines("Hello again!\nHere is a second line\nAnd a third",
            file.path(path, "test.txt"))
 diff_1 <- diff(repo)
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(identical(diff_1@old, "index"))
 stopifnot(identical(diff_1@new, "workdir"))
@@ -55,6 +57,8 @@ stopifnot(identical(length(diff_1@files[[1]]@hunks[[1]]@lines), 4L))
 ## Diff between index and HEAD is empty
 ##
 diff_2 <- diff(repo, index=TRUE)
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(identical(diff_2@old, "HEAD"))
 stopifnot(identical(diff_2@new, "index"))
@@ -64,6 +68,8 @@ stopifnot(identical(diff_2@files, list()))
 ## Diff between tree and working dir, same as diff_1
 ##
 diff_3 <- diff(tree(commits(repo)[[1]]))
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(identical(diff_3@old, tree(commits(repo)[[1]])))
 stopifnot(identical(diff_3@new, "workdir"))
@@ -74,6 +80,8 @@ stopifnot(identical(diff_3@files, diff_1@files))
 ##
 add(repo, "test.txt")
 diff_4 <- diff(repo, index=TRUE)
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(identical(diff_4@old, "HEAD"))
 stopifnot(identical(diff_4@new, "index"))
@@ -83,6 +91,8 @@ stopifnot(identical(diff_4@files, diff_1@files))
 ## Diff between tree and index
 ##
 diff_5 <- diff(tree(commits(repo)[[1]]), index=TRUE)
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(identical(diff_5@old, tree(commits(repo)[[1]])))
 stopifnot(identical(diff_5@new, "index"))
@@ -95,6 +105,8 @@ commit(repo, "Second commit")
 tree_1 <- tree(commits(repo)[[2]])
 tree_2 <- tree(commits(repo)[[1]])
 diff_6 <- diff(tree_1, tree_2)
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(identical(diff_6@old, tree_1))
 stopifnot(identical(diff_6@new, tree_2))
@@ -118,6 +130,8 @@ writeBin(as.raw((sample(0:255, 1000, replace=TRUE))),
          con=file.path(path, "test.bin"))
 add(repo, "test.bin")
 diff_7 <- diff(repo, index=TRUE)
+diff_print(repo)
+diff_print(repo, filename = file.path(path, "test.diff"))
 
 stopifnot(any(grepl("binary file", capture.output(summary(diff_7)))))
 
