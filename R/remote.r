def22103c8f913733c435efdce5cb7492c38fad5
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-26 07:55:40 +0100

    Add example to remote_remove method
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/remote.r
+++ R/remote.r
@@ -181,6 +181,33 @@ setMethod("remote_rename",
 ##' @param name The name of the remote to remove
 ##' @return NULL, invisibly
 ##' @keywords methods
+##' @examples
+##' \dontrun{
+##' ## Initialize a temporary repository
+##' path <- tempfile(pattern="git2r-")
+##' dir.create(path)
+##' repo <- init(path)
+##'
+##' ## Create a user and commit a file
+##' config(repo, user.name="User", user.email="user@@example.org")
+##' writeLines("Hello world!", file.path(path, "example.txt"))
+##' add(repo, "example.txt")
+##' commit(repo, "First commit message")
+##'
+##' ## Add a remote
+##' remote_add(repo, "playground", "https://example.org/git2r/playground")
+##' remotes(repo)
+##' remote_url(repo, "playground")
+##'
+##' ## Rename a remote
+##' remote_rename(repo, "playground", "foobar")
+##' remotes(repo)
+##' remote_url(repo, "foobar")
+##'
+##' ## Remove a remote
+##' remote_remove(repo, "foobar")
+##' remotes(repo)
+##' }
 setGeneric("remote_remove",
            signature = c("repo", "name"),
            function(repo, name)
