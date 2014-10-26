cb638b4a33ed02470b049b73740fc7e972125116
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-26 07:54:34 +0100

    Add example to remote_rename method
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/remote.r
+++ R/remote.r
@@ -126,6 +126,33 @@ setMethod("remote_add",
 ##' @param newname New name of the remote
 ##' @return NULL, invisibly
 ##' @keywords methods
+##' @examples
+##' \dontrun{
+##' ## Initialize a temporary repository
+##' path <- tempfile(pattern="git2r-")
+##' dir.create(path)
+##' repo <- init(path)
+##'
+##' ## Create a user and commit a file
+##' config(repo, user.name="User", user.email="user@@example.org")
+##' writeLines("Hello world!", file.path(path, "example.txt"))
+##' add(repo, "example.txt")
+##' commit(repo, "First commit message")
+##'
+##' ## Add a remote
+##' remote_add(repo, "playground", "https://example.org/git2r/playground")
+##' remotes(repo)
+##' remote_url(repo, "playground")
+##'
+##' ## Rename a remote
+##' remote_rename(repo, "playground", "foobar")
+##' remotes(repo)
+##' remote_url(repo, "foobar")
+##'
+##' ## Remove a remote
+##' remote_remove(repo, "foobar")
+##' remotes(repo)
+##' }
 setGeneric("remote_rename",
            signature = c("repo", "oldname", "newname"),
            function(repo, oldname, newname)
