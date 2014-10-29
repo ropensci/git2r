1519fa22baaf73c9f5bd3b35cf11d8f45d412b55
Stefan Widgren <stefan.widgren@gmail.com>
2014-10-29 21:27:08 +0100

    Add example to branch_rename method
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/branch.r
+++ R/branch.r
@@ -238,6 +238,25 @@ setMethod("branch_remote_url",
 ##' @return invisible renamed S4 class git_branch
 ##' @keywords methods
 ##' @include S4_classes.r
+##' @examples
+##' \dontrun{
+##' ## Initialize a temporary repository
+##' path <- tempfile(pattern="git2r-")
+##' dir.create(path)
+##' repo <- init(path)
+##'
+##' ## Config user and commit a file
+##' config(repo, user.name="Author", user.email="author@@example.org")
+##' writeLines("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
+##'            file.path(path, "example.txt"))
+##' add(repo, "example.txt")
+##' commit(repo, "First commit message")
+##'
+##' ## Rename 'master' branch to 'dev'
+##' branches(repo)
+##' branch_rename(head(repo), "dev")
+##' branches(repo)
+##' }
 setGeneric("branch_rename",
            signature = "branch",
            function(branch,
