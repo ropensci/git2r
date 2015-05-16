d9970e25b06cb1ed12711f06e1e659759721c0f8
Stefan Widgren <stefan.widgren@gmail.com>
2015-05-16 22:07:38 +0200

    Add punch card plot
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

---
+++ R/punch_card.r
@@ -0,0 +1,103 @@
+## git2r, R bindings to the libgit2 library.
+## Copyright (C) 2013-2015 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+##' Punch card
+##'
+##' @rdname punch_card-methods
+##' @docType methods
+##' @param repo The repository
+##' @param main Default title for the plot is "Punch card on repo:"
+##' and repository workdir basename. Supply a new title if you desire
+##' one.
+##' @param ... Additional arguments affecting the plot
+##' @return invisible NULL
+##' @keywords methods
+##' @include S4_classes.r
+##' @export
+##' @examples
+##' \dontrun{
+##' ## Initialize repository
+##' path <- tempfile(pattern="git2r-")
+##' dir.create(path)
+##' repo <- clone("https://github.com/ropensci/git2r.git", path)
+##'
+##' ## Plot
+##' punch_card(repo)
+##' }
+setGeneric("punch_card",
+           signature = "repo",
+           function(repo, main = NULL, ...)
+           standardGeneric("punch_card"))
+
+##' @rdname punch_card-methods
+##' @export
+setMethod("punch_card",
+          signature(repo = "git_repository"),
+          function (repo, main, ...)
+          {
+              savepar <- par(las = 1, mar = c(2.2, 6, 2, 0))
+              on.exit(par(savepar))
+
+              wd <- c("Saturday", "Friday", "Thursday", "Wednesday",
+                      "Tuesday", "Monday", "Sunday")
+
+              ## Extract information from repository
+              df <- as(repo, "data.frame")
+              df$when <- as.POSIXlt(df$when)
+              df$hour <- df$when$hour
+              df$weekday <- df$when$wday
+
+              ## Create a key and tabulate
+              df$key <- paste0(df$weekday, "-", df$hour)
+              df <- as.data.frame(table(df$key), stringsAsFactors = FALSE)
+              names(df) <- c("key", "Commits")
+
+              ## Convert key to Weekday and Hour
+              df$Weekday <- sapply(strsplit(df$key, "-"), "[", 1)
+              df$Weekday <- factor(df$Weekday,
+                                   levels = c(6, 5, 4, 3, 2, 1, 0),
+                                   labels = wd)
+              df$Hour <- as.integer(sapply(strsplit(df$key, "-"), "[", 2))
+              df$key <- paste0(df$Weekday, "-", df$Hour)
+
+              ## Scale
+              df$Commits <- sqrt((df$Commits / max(df$Commits)) / pi)
+
+              plot.new()
+              plot.window(xlim = c(0, 23), ylim = c(0.8, 7.2))
+              symbols(df$Hour, df$Weekday, circles = df$Commits, xaxt = "n",
+                      yaxt = "n", inches = FALSE, fg = "white", bg = "black",
+                      add = TRUE, ...)
+              h <- 0:23
+              h <- paste0(ifelse(h > 9, as.character(h),
+                                 paste0("0", as.character(h))), ":00")
+              axis(1, at = 0:23, labels = h)
+              axis(2, at = 1:7, labels = wd)
+
+              if (is.null(main)) {
+                  if (is_bare(repo)) {
+                      main <- "Punch card"
+                  } else {
+                      main <- sprintf("Punch card on repository: %s",
+                                      basename(workdir(repo)))
+                  }
+              }
+
+              title(main)
+
+              invisible(NULL)
+          }
+)
