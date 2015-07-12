475fdad4f3ca333c9452c661ae145db546ecc547
Stefan Widgren <stefan.widgren@gmail.com>
2015-07-12 22:17:51 +0200

    Add C code for argument check of S4 class 'cred_env'
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- src/git2r_arg.c
+++ src/git2r_arg.c
@@ -120,7 +120,13 @@ int git2r_arg_check_credentials(SEXP arg)
         return -1;
 
     class_name = getAttrib(arg, R_ClassSymbol);
-    if (0 == strcmp(CHAR(STRING_ELT(class_name, 0)), "cred_user_pass")) {
+    if (0 == strcmp(CHAR(STRING_ELT(class_name, 0)), "cred_env")) {
+        /* Check username and password */
+        if (git2r_arg_check_string(GET_SLOT(arg, Rf_install("username"))))
+            return -1;
+        if (git2r_arg_check_string(GET_SLOT(arg, Rf_install("password"))))
+            return -1;
+    } else if (0 == strcmp(CHAR(STRING_ELT(class_name, 0)), "cred_user_pass")) {
         /* Check username and password */
         if (git2r_arg_check_string(GET_SLOT(arg, Rf_install("username"))))
             return -1;
