867d78621f0e4b550e9f9c34a926e0430ba13ccd
Stefan Widgren <stefan.widgren@gmail.com>
2015-07-27 10:34:16 +0200

    Update configure.win to download libssh2
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- tools/winlibs.R
+++
@@ -1,8 +0,0 @@
-if (!file.exists("../windows/libssh2-1.6.0/include/libssh2.h")) {
-    setInternet2()
-    download.file("https://github.com/rwinlib/libssh2/archive/v1.6.0.zip",
-                  "lib.zip", quiet = TRUE)
-    dir.create("../windows", showWarnings = FALSE)
-    unzip("lib.zip", exdir = "../windows")
-    unlink("lib.zip")
-}
