160325b55cb57aa74e36dc7ec63eeb419cdf10e2
Stefan Widgren <stefan.widgren@gmail.com>
2015-09-30 06:55:28 +0200

    Update libgit2_sha
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/libgit2.r
+++ R/libgit2.r
@@ -47,4 +47,4 @@ libgit2_version <- function() {
 ##' @export
 ##' @examples
 ##' libgit2_sha()
-libgit2_sha <- function() "aebddbe7367a16c8ac6cc5638ae46ba8063d2213"
+libgit2_sha <- function() "c628ebca63316954b012fc9469e97513759d0353"
