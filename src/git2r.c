a76cfd6065b6f6791db368b935c0bdba15be9ba1
Stefan Widgren <stefan.widgren@gmail.com>
2017-03-03 18:08:35 +0100

    Add call to 'R_forceSymbols'
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- src/git2r.c
+++ src/git2r.c
@@ -1,6 +1,6 @@
 /*
  *  git2r, R bindings to the libgit2 library.
- *  Copyright (C) 2013-2016 The git2r contributors
+ *  Copyright (C) 2013-2017 The git2r contributors
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License, version 2,
@@ -164,6 +164,7 @@ R_init_git2r(DllInfo *info)
 {
     R_registerRoutines(info, NULL, callMethods, NULL, NULL);
     R_useDynamicSymbols(info, FALSE);
+    R_forceSymbols(info, TRUE);
     git_libgit2_init();
 }
 
