5d72b62bf0a99901964a66db8087e75a1c32913b
Thierry Onkelinx <Thierry.Onkelinx@inbo.be>
2017-12-10 14:49:54 +0100

    test read_delim_git()
    Signed-off-by: Thierry Onkelinx <Thierry.Onkelinx@inbo.be>

---
+++ tests/testthat/test_read_delim_git.R
@@ -0,0 +1,61 @@
+context("read data.frame from git")
+describe("read_delim_git()", {
+  file <- "test.txt"
+  local.path <- "test"
+  connection <- normalizePath(
+    tempfile(pattern = "git2r-"),
+    winslash = "/",
+    mustWork = FALSE
+  )
+  df <- data.frame(x = 1, y = 1:10)
+
+
+  it("stops if connection is not a git repository", {
+    expect_error(
+      read_delim_git(
+        file = file,
+        local.path = local.path,
+        connection = connection,
+        commit.user = "me",
+        commit.email = "me@me.com"
+      ),
+      "is not a git repo"
+    )
+  })
+
+  dir.create(paste(connection, local.path, sep = "/"), recursive = TRUE)
+  repo <- init(connection)
+
+  it("returns FALSE when the file doesn't exists", {
+    expect_error(
+      read_delim_git(
+        file = file,
+        local.path = local.path,
+        connection = connection,
+        commit.user = "me",
+        commit.email = "me@me.com"
+      ),
+      paste0(repo@path, "/", local.path, "/", file)
+    )
+  })
+  write_delim_git(
+    x = df,
+    file = file,
+    local.path = local.path,
+    connection = connection,
+    commit.user = "me",
+    commit.email = "me@me.com"
+  )
+  it("read the tab-delimited file", {
+    expect_that(
+      read_delim_git(
+        file = file,
+        local.path = local.path,
+        connection = connection,
+        commit.user = "me",
+        commit.email = "me@me.com"
+      ),
+      is_equivalent_to(df)
+    )
+  })
+})
