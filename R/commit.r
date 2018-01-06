2bce10e990899b8db502f7865ac36480859b762d
Stefan Widgren <stefan.widgren@gmail.com>
2018-01-06 10:19:24 +0100

    Add roxygen template for repo parameter
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/commit.r
+++ R/commit.r
@@ -314,12 +314,7 @@ setMethod("commits",
 ##' Last commit
 ##'
 ##' Get last commit in the current branch.
-##' @param repo repository \code{object}
-##'     \code{\linkS4class{git_repository}}, or a path to a
-##'     repository, or \code{NULL}.  If the \code{repo} argument is
-##'     \code{NULL}, the repository is searched for with
-##'     \code{\link{discover_repository}} in the current working
-##'     directory.
+##' @template repo-param
 ##' @export
 ##' @examples
 ##' \dontrun{
---
+++ man-roxygen/repo-param.R
@@ -0,0 +1,6 @@
+##' @param repo repository \code{object}
+##'     \code{\linkS4class{git_repository}}, or a path to a
+##'     repository, or \code{NULL}.  If the \code{repo} argument is
+##'     \code{NULL}, the repository is searched for with
+##'     \code{\link{discover_repository}} in the current working
+##'     directory.
