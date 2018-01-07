02719c867575da55be8df71f603b776a71f304c9
Stefan Widgren <stefan.widgren@gmail.com>
2018-01-07 11:34:37 +0100

    Roxygenize

--- man-roxygen/repo-param.R
+++ man-roxygen/repo-param.R
@@ -1,6 +1,5 @@
-##' @param repo repository \code{object}
-##'     \code{\linkS4class{git_repository}}, or a path to a
-##'     repository, or \code{NULL}.  If the \code{repo} argument is
-##'     \code{NULL}, the repository is searched for with
+##' @param repo a \code{\linkS4class{git_repository}} object, or a
+##'     path to a repository, or \code{NULL}.  If the \code{repo}
+##'     argument is \code{NULL}, the repository is searched for with
 ##'     \code{\link{discover_repository}} in the current working
 ##'     directory.
