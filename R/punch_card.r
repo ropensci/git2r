29a7b3f096a0ad6ca370d71fc66e05a928fd3341
Stefan Widgren <stefan.widgren@gmail.com>
2018-01-10 17:52:51 +0100

    Refactoring of 'punch_card' function
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- R/punch_card.r
+++ R/punch_card.r
@@ -16,16 +16,12 @@
 
 ##' Punch card
 ##'
-##' @rdname punch_card-methods
-##' @docType methods
-##' @param repo The repository
+##' @template repo-param
 ##' @param main Default title for the plot is "Punch card on repo:"
-##' and repository workdir basename. Supply a new title if you desire
-##' one.
+##'     and repository workdir basename. Supply a new title if you
+##'     desire one.
 ##' @param ... Additional arguments affecting the plot
 ##' @return invisible NULL
-##' @keywords methods
-##' @include S4_classes.r
 ##' @importFrom graphics axis
 ##' @importFrom graphics par
 ##' @importFrom graphics plot.new
@@ -42,67 +38,54 @@
 ##' ## Plot
 ##' punch_card(repo)
 ##' }
-setGeneric("punch_card",
-           signature = "repo",
-           function(repo, main = NULL, ...)
-           standardGeneric("punch_card"))
+punch_card <- function(repo = NULL, main = NULL, ...) {
+    savepar <- graphics::par(las = 1, mar = c(2.2, 6, 2, 0))
+    on.exit(par(savepar))
 
-##' @rdname punch_card-methods
-##' @export
-setMethod("punch_card",
-          signature(repo = "git_repository"),
-          function(repo, main, ...)
-          {
-              savepar <- graphics::par(las = 1, mar = c(2.2, 6, 2, 0))
-              on.exit(par(savepar))
+    wd <- c("Saturday", "Friday", "Thursday", "Wednesday", "Tuesday",
+            "Monday", "Sunday")
 
-              wd <- c("Saturday", "Friday", "Thursday", "Wednesday",
-                      "Tuesday", "Monday", "Sunday")
+    ## Extract information from repository
+    repo <- lookup_repository(repo)
+    df <- as(repo, "data.frame")
+    df$when <- as.POSIXlt(df$when)
+    df$hour <- df$when$hour
+    df$weekday <- df$when$wday
 
-              ## Extract information from repository
-              df <- as(repo, "data.frame")
-              df$when <- as.POSIXlt(df$when)
-              df$hour <- df$when$hour
-              df$weekday <- df$when$wday
+    ## Create a key and tabulate
+    df$key <- paste0(df$weekday, "-", df$hour)
+    df <- as.data.frame(table(df$key), stringsAsFactors = FALSE)
+    names(df) <- c("key", "Commits")
 
-              ## Create a key and tabulate
-              df$key <- paste0(df$weekday, "-", df$hour)
-              df <- as.data.frame(table(df$key), stringsAsFactors = FALSE)
-              names(df) <- c("key", "Commits")
+    ## Convert key to Weekday and Hour
+    df$Weekday <- sapply(strsplit(df$key, "-"), "[", 1)
+    df$Weekday <- factor(df$Weekday, levels = c(6, 5, 4, 3, 2, 1, 0), labels = wd)
+    df$Hour <- as.integer(sapply(strsplit(df$key, "-"), "[", 2))
+    df$key <- paste0(df$Weekday, "-", df$Hour)
 
-              ## Convert key to Weekday and Hour
-              df$Weekday <- sapply(strsplit(df$key, "-"), "[", 1)
-              df$Weekday <- factor(df$Weekday,
-                                   levels = c(6, 5, 4, 3, 2, 1, 0),
-                                   labels = wd)
-              df$Hour <- as.integer(sapply(strsplit(df$key, "-"), "[", 2))
-              df$key <- paste0(df$Weekday, "-", df$Hour)
+    ## Scale
+    df$Commits <- sqrt((df$Commits / max(df$Commits)) / pi)
 
-              ## Scale
-              df$Commits <- sqrt((df$Commits / max(df$Commits)) / pi)
+    graphics::plot.new()
+    graphics::plot.window(xlim = c(0, 23), ylim = c(0.8, 7.2))
+    graphics::symbols(df$ Hour, df$Weekday, circles = df$Commits,
+                      xaxt = "n", yaxt = "n", inches = FALSE,
+                      fg = "white", bg = "black", add = TRUE, ...)
+    h <- 0:23
+    h <- paste0(ifelse(h > 9, as.character(h), paste0("0", as.character(h))), ":00")
+    graphics::axis(1, at = 0:23, labels = h)
+    graphics::axis(2, at = 1:7, labels = wd)
 
-              graphics::plot.new()
-              graphics::plot.window(xlim = c(0, 23), ylim = c(0.8, 7.2))
-              graphics::symbols(df$Hour, df$Weekday, circles = df$Commits,
-                                xaxt = "n", yaxt = "n", inches = FALSE,
-                                fg = "white", bg = "black", add = TRUE, ...)
-              h <- 0:23
-              h <- paste0(ifelse(h > 9, as.character(h),
-                                 paste0("0", as.character(h))), ":00")
-              graphics::axis(1, at = 0:23, labels = h)
-              graphics::axis(2, at = 1:7, labels = wd)
+    if (is.null(main)) {
+        if (is_bare(repo)) {
+            main <- "Punch card"
+        } else {
+            main <- sprintf("Punch card on repository: %s",
+                            basename(workdir(repo)))
+        }
+    }
 
-              if (is.null(main)) {
-                  if (is_bare(repo)) {
-                      main <- "Punch card"
-                  } else {
-                      main <- sprintf("Punch card on repository: %s",
-                                      basename(workdir(repo)))
-                  }
-              }
+    graphics::title(main)
 
-              graphics::title(main)
-
-              invisible(NULL)
-          }
-)
+    invisible(NULL)
+}
