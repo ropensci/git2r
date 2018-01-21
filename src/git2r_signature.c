843d19b6a927915b423fb2d0994242bfcc7d11da
Stefan Widgren <stefan.widgren@gmail.com>
2018-01-21 19:55:02 +0100

    Change git2r C code for a git_signature object to S3
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- src/git2r_signature.c
+++ src/git2r_signature.c
@@ -1,6 +1,6 @@
 /*
  *  git2r, R bindings to the libgit2 library.
- *  Copyright (C) 2013-2017 The git2r contributors
+ *  Copyright (C) 2013-2018 The git2r contributors
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License, version 2,
@@ -16,20 +16,20 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <Rdefines.h>
 #include "git2r_error.h"
+#include "git2r_objects.h"
 #include "git2r_repository.h"
 #include "git2r_signature.h"
 
 /**
  * Get the configured signature for a repository
  *
- * @param repo S4 class git_repository
- * @return S4 class git_signature
+ * @param repo S3 class git_repository
+ * @return S3 class git_signature
  */
 SEXP git2r_signature_default(SEXP repo)
 {
-    int err;
+    int error, nprotect = 0;
     git_repository *repository = NULL;
     git_signature *signature = NULL;
     SEXP result = R_NilValue;
@@ -38,24 +38,24 @@ SEXP git2r_signature_default(SEXP repo)
     if (!repository)
         git2r_error(__func__, NULL, git2r_err_invalid_repository, NULL);
 
-    err = git_signature_default(&signature, repository);
-    if (err)
+    error = git_signature_default(&signature, repository);
+    if (error)
         goto cleanup;
 
-    PROTECT(result = NEW_OBJECT(MAKE_CLASS("git_signature")));
+    PROTECT(result = Rf_mkNamed(VECSXP, git2r_S3_items__git_signature));
+    nprotect++;
+    Rf_setAttrib(result, R_ClassSymbol,
+                 Rf_mkString(git2r_S3_class__git_signature));
     git2r_signature_init(signature, result);
 
 cleanup:
-    if (repository)
-        git_repository_free(repository);
+    git_repository_free(repository);
+    git_signature_free(signature);
 
-    if (signature)
-        git_signature_free(signature);
+    if (nprotect)
+        UNPROTECT(nprotect);
 
-    if (!Rf_isNull(result))
-        UNPROTECT(1);
-
-    if (err)
+    if (error)
         git2r_error(__func__, giterr_last(), NULL, NULL);
 
     return result;
@@ -65,44 +65,59 @@ cleanup:
  * Init signature from SEXP signature argument
  *
  * @param out new signature
- * @param signature SEXP argument with S4 class git_signature
- * @return void
+ * @param signature SEXP argument with S3 class git_signature
+ * @return 0 or an error code
  */
 int git2r_signature_from_arg(git_signature **out, SEXP signature)
 {
-    int err;
-    SEXP when;
+    SEXP when = git2r_get_list_element(signature, "when");
 
-    when = GET_SLOT(signature, Rf_install("when"));
-    err = git_signature_new(
+    return git_signature_new(
         out,
-        CHAR(STRING_ELT(GET_SLOT(signature, Rf_install("name")), 0)),
-        CHAR(STRING_ELT(GET_SLOT(signature, Rf_install("email")), 0)),
-        REAL(GET_SLOT(when, Rf_install("time")))[0],
-        REAL(GET_SLOT(when, Rf_install("offset")))[0]);
-
-    return err;
+        CHAR(STRING_ELT(git2r_get_list_element(signature, "name"), 0)),
+        CHAR(STRING_ELT(git2r_get_list_element(signature, "email"), 0)),
+        REAL(git2r_get_list_element(when, "time"))[0],
+        REAL(git2r_get_list_element(when, "offset"))[0]);
 }
 
 /**
- * Init slots in S4 class git_signature.
+ * Init slots in S3 class git_signature.
  *
  * @param source A git signature
- * @param dest S4 class git_signature to initialize
+ * @param dest S3 class git_signature to initialize
  * @return void
  */
 void git2r_signature_init(const git_signature *source, SEXP dest)
 {
     SEXP when;
-    SEXP s_name = Rf_install("name");
-    SEXP s_email = Rf_install("email");
-    SEXP s_time = Rf_install("time");
-    SEXP s_offset = Rf_install("offset");
 
-    SET_SLOT(dest, s_name, Rf_mkString(source->name));
-    SET_SLOT(dest, s_email, Rf_mkString(source->email));
+    SET_VECTOR_ELT(
+        dest,
+        git2r_S3_item__git_signature__name,
+        Rf_mkString(source->name));
 
-    when = GET_SLOT(dest, Rf_install("when"));
-    SET_SLOT(when, s_time, Rf_ScalarReal((double)source->when.time));
-    SET_SLOT(when, s_offset, Rf_ScalarReal((double)source->when.offset));
+    SET_VECTOR_ELT(
+        dest,
+        git2r_S3_item__git_signature__email,
+        Rf_mkString(source->email));
+
+    when = VECTOR_ELT(dest, git2r_S3_item__git_signature__when);
+    if (Rf_isNull(when)) {
+        SET_VECTOR_ELT(
+            dest,
+            git2r_S3_item__git_signature__when,
+            when = Rf_mkNamed(VECSXP, git2r_S3_items__git_time));
+        Rf_setAttrib(when, R_ClassSymbol,
+                     Rf_mkString(git2r_S3_class__git_time));
+    }
+
+    SET_VECTOR_ELT(
+        when,
+        git2r_S3_item__git_time__time,
+        Rf_ScalarReal((double)source->when.time));
+
+    SET_VECTOR_ELT(
+        when,
+        git2r_S3_item__git_time__offset,
+        Rf_ScalarReal((double)source->when.offset));
 }
