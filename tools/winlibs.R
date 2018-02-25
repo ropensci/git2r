70cbd3ed0cfc9831ac34c103c2518ed4322dca20
Jeroen Ooms <jeroenooms@gmail.com>
2018-02-25 14:16:04 +0100

    Update libssh2 on Windows and cleanup build. Fixes #327

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,25 +1,10 @@
 # We need https
 if(getRversion() < "3.3.0") setInternet2()
+LIBSSH2 <- commandArgs(TRUE)
 
-# Download libz
-if(!file.exists("../windows/libz-1.2.8/include/zlib.h")){
-  download.file("https://github.com/rwinlib/libz/archive/v1.2.8.zip", "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
-}
-
-# Download OpenSSL
-if(!file.exists("../windows/openssl-1.0.2d/include/openssl/ssl.h")){
-  download.file("https://github.com/rwinlib/openssl/archive/v1.0.2d.zip", "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
-}
-
-# Download libssh2
-if(!file.exists("../windows/libssh2-1.6.0/include/libssh2.h")){
-  download.file("https://github.com/rwinlib/libssh2/archive/v1.6.0.zip", "lib.zip", quiet = TRUE)
+# Downloads libssh2 + dependencies
+if(!file.exists(sprintf("../windows/libssh2-%sinclude/libssh2.h", LIBSSH2))){
+  download.file(sprintf("https://github.com/rwinlib/libssh2/archive/v%s.zip", LIBSSH2), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
