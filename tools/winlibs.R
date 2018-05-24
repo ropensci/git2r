af199ed106618909dc0f4fd7a8f7475a31488a5e
Jeroen Ooms <jeroenooms@gmail.com>
2018-05-24 15:44:26 +0200

    Windows: switch to prebuilt libgit2

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,10 +1,10 @@
 # We need https
 if(getRversion() < "3.3.0") setInternet2()
-LIBSSH2 <- commandArgs(TRUE)
+VERSION <- commandArgs(TRUE)
 
 # Downloads libssh2 + dependencies
-if(!file.exists(sprintf("../windows/libssh2-%sinclude/libssh2.h", LIBSSH2))){
-  download.file(sprintf("https://github.com/rwinlib/libssh2/archive/v%s.zip", LIBSSH2), "lib.zip", quiet = TRUE)
+if(!file.exists(sprintf("../windows/libgit2-%s/include/git2.h", VERSION))){
+  download.file(sprintf("https://github.com/rwinlib/libgit2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
