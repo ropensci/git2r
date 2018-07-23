03d8f6ddd1d6e13900a987393d20279fc459d6f0
ThierryO <thierry.onkelinx@inbo.be>
2018-07-23 14:25:49 +0200

    Add write_delim_git() Signed-off-by: Thierry Onkelinx <thierry.onkelinx@inbo.be>

---
+++ R/write_delim_git.R
@@ -0,0 +1,56 @@
+## Copyright (C) 2013-2018 The git2r contributors
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License, version 2,
+## as published by the Free Software Foundation.
+##
+## git2r is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+##' Write a \code{data.frame} to a git repository
+##'
+##' This will create two files. The \code{".tsv"} file contains the raw data.
+##' The \code{".yml"} contains the meta data on the columns in YAML format.
+##' @param x the \code{data.frame}
+##' @param file the name of the file with file extension. Can include a path
+##' relative to the path of the \code{repo}
+##' @param repo a \code{git_repository} object, created with
+##' \code{\link{repository}}
+##' @export
+##' @include meta.R
+##' @importFrom utils write.table
+write_delim_git <- function(x, file, repo) {
+  if (!inherits(x, "data.frame")) {
+    stop("x is not a 'data.frame'")
+  }
+  if (grepl("\\..*$", file)) {
+    warning("file extensions are stripped")
+    file <- gsub("\\..*$", "", file)
+  }
+  if (!inherits(repo, "git_repository")) {
+    stop("repo is not a 'git_repository'")
+  }
+  raw_file <- sprintf("%s/%s.tsv", dirname(repo$path), file)
+  meta_file <- sprintf("%s/%s.yml", dirname(repo$path), file)
+  if (!dir.exists(dirname(raw_file))) {
+    dir.create(dirname(raw_file), recursive = TRUE)
+  }
+  raw_data <- as.data.frame(lapply(x, meta), stringsAsFactors = FALSE)
+  meta_data <- paste(
+    colnames(x),
+    vapply(raw_data, attr, "", which = "meta"),
+    sep = ":\n"
+  )
+  writeLines(meta_data, meta_file)
+  write.table(
+    x = raw_data, file = raw_file, append = FALSE,
+    quote = FALSE, sep = "\t", eol = "\n", dec = ".",
+    row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8"
+  )
+}
