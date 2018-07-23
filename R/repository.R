18b38bbc39fbc8aab45d224df212608385b39ad5
ThierryO <thierry.onkelinx@inbo.be>
2018-07-23 19:52:52 +0200

    init() and clone() gain the "project" argument
    Signed-off-by: Thierry Onkelinx <thierry.onkelinx@inbo.be>

--- R/repository.R
+++ R/repository.R
@@ -191,6 +191,7 @@ repository <- function(path = ".", discover = TRUE, project = NULL) {
 ##'     is created at the pointed path. If FALSE, provided path will
 ##'     be considered as the working directory into which the .git
 ##'     directory will be created.
+##' @inheritParams repository
 ##' @return A \code{git_repository} object
 ##' @export
 ##' @seealso \link{repository}
@@ -208,12 +209,12 @@ repository <- function(path = ".", discover = TRUE, project = NULL) {
 ##' repo_bare <- init(path_bare, bare = TRUE)
 ##' is_bare(repo_bare)
 ##' }
-init <- function(path = ".", bare = FALSE) {
+init <- function(path = ".", bare = FALSE, project = NULL) {
     path <- normalizePath(path, winslash = "/", mustWork = TRUE)
     if (!file.info(path)$isdir)
         stop("'path' is not a directory")
     .Call(git2r_repository_init, path, bare)
-    repository(path)
+    repository(path, project = project)
 }
 
 ##' Clone a remote repository
@@ -229,6 +230,7 @@ init <- function(path = ".", bare = FALSE) {
 ##'     access. Default is NULL. To use and query an ssh-agent for the
 ##'     ssh key credentials, let this parameter be NULL (the default).
 ##' @param progress Show progress. Default is TRUE.
+##' @inheritParams repository
 ##' @return A \code{git_repository} object.
 ##' @seealso \link{repository}, \code{\link{cred_user_pass}},
 ##'     \code{\link{cred_ssh_key}}
@@ -279,11 +281,12 @@ clone <- function(url         = NULL,
                   branch      = NULL,
                   checkout    = TRUE,
                   credentials = NULL,
+                  project     = NULL,
                   progress    = TRUE)
 {
     .Call(git2r_clone, url, local_path, bare,
           branch, checkout, credentials, progress)
-    repository(local_path)
+    repository(local_path, project = project)
 }
 
 ##' Get HEAD for a repository
