d7d9e61603f4394fc97580ed4769fb875b1825e3
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-01-08 14:27:43 -0600

    Additional simplifications of log.c.

--- inst/code/log.c
+++ inst/code/log.c
@@ -14,81 +14,39 @@
 
 #include "common.h"
 
-/**
- * This example demonstrates the libgit2 rev walker APIs to roughly
- * simulate the output of `git log` and a few of command line arguments.
- * `git log` has many many options and this only shows a few of them.
- *
- * This does not have:
- *
- * - Robust error handling
- * - Colorized or paginated output formatting
- * - Most of the `git log` options
- *
- * This does have:
- *
- * - Examples of translating command line arguments to equivalent libgit2
- *   revwalker configuration calls
- * - Simplified options to apply pathspec limits and to show basic diffs
- */
-
-/** log_state represents walker being configured while handling options */
-struct log_state {
-  git_repository *repo;
-  const char *repodir;
-  git_revwalk *walker;
-  int hide;
-  int sorting;
-  int revisions;
-};
-
-/** log_options holds other command line options that affect log output */
-struct log_options {
-  int show_log_size;
-  int skip, limit;
-  int min_parents, max_parents;
-  git_time_t before;
-  git_time_t after;
-  const char *author;
-  const char *committer;
-  const char *grep;
-};
-
-/** utility functions that parse options and help with log output */
-static void init_options(struct log_state *s, struct log_options *opt);
-static void print_commit(git_commit *commit, struct log_options *opts);
-static int match_with_parent(git_commit *commit, int i, git_diff_options *);
+static void print_commit (git_commit *commit);
+static int match_with_parent (git_commit *commit, int i,
+			      git_diff_options *opts);
 
 int main(int argc, char *argv[]) {
   int i, unmatched;
   int count   = 0;
   int printed = 0;
   int parents = 0;
-  struct log_state s;
-  struct log_options opt;
   git_diff_options diffopts = GIT_DIFF_OPTIONS_INIT;
   git_oid oid;
-  git_commit *commit = NULL;
-  git_pathspec *ps = NULL;
+  git_repository *repo = NULL;
+  git_revwalk *walker  = NULL;
+  git_commit *commit   = NULL;
+  git_pathspec *ps     = NULL;
   git_tree *tree;
 	
   git_libgit2_init();
   
   // Parse arguments and set up revwalker.
-  init_options(&s,&opt);
   diffopts.pathspec.strings = &argv[1];
   diffopts.pathspec.count   = 1;
   git_pathspec_new(&ps, &diffopts.pathspec);
 
   // Open the repository.
-  git_repository_open_ext(&s.repo, ".", 0, NULL);
-  git_revwalk_new(&s.walker,s.repo);
-  git_revwalk_sorting(s.walker, s.sorting);
-  git_revwalk_push_head(s.walker);
+  git_repository_open_ext(&repo, ".", 0, NULL);
+  git_revwalk_new(&walker,repo);
+  git_revwalk_sorting(walker,GIT_SORT_TIME);
+  git_revwalk_push_head(walker);
   
   // Use the revwalker to traverse the history.
-  for (; !git_revwalk_next(&oid, s.walker); git_commit_free(commit)) {
-    git_commit_lookup(&commit, s.repo, &oid);
+  for (; !git_revwalk_next(&oid, walker); git_commit_free(commit)) {
+    git_commit_lookup(&commit, repo, &oid);
     
     parents = (int) git_commit_parentcount(commit);
     unmatched = parents;
@@ -111,12 +69,12 @@ int main(int argc, char *argv[]) {
     if (unmatched > 0)
       continue;
     
-    print_commit(commit, &opt);
+    print_commit(commit);
   }
   
   git_pathspec_free(ps);
-  git_revwalk_free(s.walker);
-  git_repository_free(s.repo);
+  git_revwalk_free(walker);
+  git_repository_free(repo);
   git_libgit2_shutdown();
   
   return 0;
@@ -124,7 +82,7 @@ int main(int argc, char *argv[]) {
 
 
 /** Helper to print a commit object. */
-static void print_commit(git_commit *commit, struct log_options *opts) {
+static void print_commit (git_commit *commit) {
   char buf[GIT_OID_HEXSZ + 1];
   int i, count;
   const git_signature *sig;
@@ -135,8 +93,8 @@ static void print_commit(git_commit *commit, struct log_options *opts) {
 }
 
 /** Helper to find how many files in a commit changed from its nth parent. */
-static int match_with_parent(git_commit *commit, int i,
-			     git_diff_options *opts) {
+static int match_with_parent (git_commit *commit, int i,
+			      git_diff_options *opts) {
   git_commit *parent;
   git_tree *a, *b;
   git_diff *diff;
@@ -147,7 +105,7 @@ static int match_with_parent(git_commit *commit, int i,
   git_commit_tree(&b, commit);
   git_diff_tree_to_tree(&diff, git_commit_owner(commit), a, b, opts);
   
-  ndeltas = (int)git_diff_num_deltas(diff);
+  ndeltas = (int) git_diff_num_deltas(diff);
   
   git_diff_free(diff);
   git_tree_free(a);
@@ -155,15 +113,4 @@ static int match_with_parent(git_commit *commit, int i,
   git_commit_free(parent);
   
   return ndeltas > 0;
-}
-
-/** Initialize the log options. */
-static void init_options(struct log_state *s, struct log_options *opt) {
-  
-  memset(s, 0, sizeof(*s));
-  s->sorting = GIT_SORT_TIME;
-  
-  memset(opt, 0, sizeof(*opt));
-  opt->max_parents = -1;
-  opt->limit = -1;
 }
