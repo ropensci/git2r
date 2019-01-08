c51ab02873d6ec44a41e92635ba14f8f25ba1c5c
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-01-08 12:09:48 -0600

    More simplifications to log.c.

--- inst/code/log.c
+++ inst/code/log.c
@@ -43,7 +43,6 @@ struct log_state {
 };
 
 /** utility functions that are called to configure the walker */
-static void set_sorting(struct log_state *s, unsigned int sort_mode);
 static void push_rev(struct log_state *s, git_object *obj, int hide);
 static int add_revision(struct log_state *s, const char *revstr);
 
@@ -61,7 +60,6 @@ struct log_options {
 
 /** utility functions that parse options and help with log output */
 static void init_options(struct log_state *s, struct log_options *opt);
-static void print_time(const git_time *intime, const char *prefix);
 static void print_commit(git_commit *commit, struct log_options *opts);
 static int match_with_parent(git_commit *commit, int i, git_diff_options *);
 
@@ -186,53 +184,6 @@ static int add_revision(struct log_state *s, const char *revstr) {
   return 0;
 }
 
-/** Update revwalker with sorting mode. */
-static void set_sorting(struct log_state *s, unsigned int sort_mode) {
-  
-  /** Open repo on demand if it isn't already open. */
-  if (!s->repo) {
-    if (!s->repodir) s->repodir = ".";
-    git_repository_open_ext(&s->repo, s->repodir, 0, NULL);
-  }
-  
-  /** Create revwalker on demand if it doesn't already exist. */
-  if (!s->walker)
-    git_revwalk_new(&s->walker, s->repo);
-  
-  if (sort_mode == GIT_SORT_REVERSE)
-    s->sorting = s->sorting ^ GIT_SORT_REVERSE;
-  else
-    s->sorting = sort_mode | (s->sorting & GIT_SORT_REVERSE);
-  
-  git_revwalk_sorting(s->walker, s->sorting);
-}
-
-/** Helper to format a git_time value like Git. */
-static void print_time(const git_time *intime, const char *prefix) {
-  char sign, out[32];
-  struct tm *intm;
-  int offset, hours, minutes;
-  time_t t;
-
-  offset = intime->offset;
-  if (offset < 0) {
-    sign = '-';
-    offset = -offset;
-  } else {
-    sign = '+';
-  }
-  
-  hours   = offset / 60;
-  minutes = offset % 60;
-  
-  t = (time_t)intime->time + (intime->offset * 60);
-  
-  intm = gmtime(&t);
-  strftime(out, sizeof(out), "%a %b %e %T %Y", intm);
-  
-  printf("%s%s %c%02d%02d\n", prefix, out, sign, hours, minutes);
-}
-
 /** Helper to print a commit object. */
 static void print_commit(git_commit *commit, struct log_options *opts) {
   char buf[GIT_OID_HEXSZ + 1];
@@ -241,30 +192,7 @@ static void print_commit(git_commit *commit, struct log_options *opts) {
   const char *scan, *eol;
   
   git_oid_tostr(buf, sizeof(buf), git_commit_id(commit));
-  printf("commit %s\n", buf);
-  
-  if ((count = (int)git_commit_parentcount(commit)) > 1) {
-    printf("Merge:");
-    for (i = 0; i < count; ++i) {
-      git_oid_tostr(buf, 8, git_commit_parent_id(commit, i));
-      printf(" %s", buf);
-    }
-    printf("\n");
-  }
-
-  if ((sig = git_commit_author(commit)) != NULL) {
-    printf("Author: %s <%s>\n", sig->name, sig->email);
-    print_time(&sig->when, "Date:   ");
-  }
-  printf("\n");
-  
-  for (scan = git_commit_message(commit); scan && *scan; ) {
-    for (eol = scan; *eol && *eol != '\n'; ++eol) /* find eol */;
-    
-    printf("    %.*s\n", (int)(eol - scan), scan);
-    scan = *eol ? eol + 1 : NULL;
-  }
-  printf("\n");
+  printf("%s\n", buf);
 }
 
 /** Helper to find how many files in a commit changed from its nth parent. */
@@ -275,12 +203,11 @@ static int match_with_parent(git_commit *commit, int i,
   git_diff *diff;
   int ndeltas;
   
-  check_lg2(git_commit_parent(&parent, commit, (size_t)i), "Get parent", NULL);
-  check_lg2(git_commit_tree(&a, parent), "Tree for parent", NULL);
-  check_lg2(git_commit_tree(&b, commit), "Tree for commit", NULL);
-  check_lg2(git_diff_tree_to_tree(&diff, git_commit_owner(commit), a, b, opts),
-	    "Checking diff between parent and commit", NULL);
-
+  git_commit_parent(&parent, commit, (size_t) i);
+  git_commit_tree(&a, parent);
+  git_commit_tree(&b, commit);
+  git_diff_tree_to_tree(&diff, git_commit_owner(commit), a, b, opts);
+  
   ndeltas = (int)git_diff_num_deltas(diff);
   
   git_diff_free(diff);
@@ -289,16 +216,6 @@ static int match_with_parent(git_commit *commit, int i,
   git_commit_free(parent);
   
   return ndeltas > 0;
-}
-
-/** Print a usage message for the program. */
-static void usage(const char *message, const char *arg) {
-  if (message && arg)
-    fprintf(stderr, "%s: %s\n", message, arg);
-  else if (message)
-    fprintf(stderr, "%s\n", message);
-  fprintf(stderr, "usage: log [<options>]\n");
-  exit(1);
 }
 
 /** Initialize the log options. */
