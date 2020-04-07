24a3c7409833b560b6de0f728f9e5368e3235bb9
Stefan Widgren <stefan.widgren@gmail.com>
2020-04-07 19:26:23 +0200

    Fix documentation

--- R/git2r.R
+++ R/git2r.R
@@ -1,5 +1,5 @@
 ## git2r, R bindings to the libgit2 library.
-## Copyright (C) 2013-2018 The git2r contributors
+## Copyright (C) 2013-2020 The git2r contributors
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License, version 2,
@@ -16,6 +16,7 @@
 
 ##' git2r: R bindings to the libgit2 library
 ##'
+##' git2r: R bindings to the libgit2 library.
 ##' @docType package
 ##' @name git2r
 ##' @useDynLib git2r, .registration=TRUE
