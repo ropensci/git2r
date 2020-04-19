163d62a98d980678d5eac7513287346f1a2cc285
Stefan Widgren <stefan.widgren@gmail.com>
2020-04-19 18:40:24 +0200

    configure: update for libgit2 v0.99.0
    Signed-off-by: Stefan Widgren <stefan.widgren@gmail.com>

--- tools/version.c
+++ tools/version.c
@@ -4,6 +4,6 @@
 #    if LIBGIT2_VER_MINOR < 26
 #        error libgit2 version too old
 #    endif
-#else
+#elif LIBGIT2_VER_MAJOR > 1
 #    error the libgit2 version is not compatible with git2r
 #endif
