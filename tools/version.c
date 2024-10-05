6e896c0531959fab8837bacac0d34b0fca177864
Stefan Widgren <stefan.widgren@gmail.com>
2024-10-05 13:19:50 +0200

    Require libgit2 version >= 1.0

--- tools/version.c
+++ tools/version.c
@@ -1,9 +1,5 @@
 #include <git2.h>
 
-#if LIBGIT2_VER_MAJOR == 0
-#    if LIBGIT2_VER_MINOR < 26
-#        error libgit2 version too old
-#    endif
-#elif LIBGIT2_VER_MAJOR > 1
-#    error the libgit2 version is not compatible with git2r
+#if LIBGIT2_VER_MAJOR < 1
+#    error libgit2 version too old
 #endif
