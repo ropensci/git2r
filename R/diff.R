49c9737145bb7ad4f5e7101317b793658c8f88b9
Stefan Widgren <stefan.widgren@gmail.com>
2024-10-19 16:05:31 +0200

    Revert "Cleanup exports"
    This reverts commit 03a58a443cdc98f1cc784bdfe0dfc87768bf3d8f.

--- R/diff.R
+++ R/diff.R
@@ -300,3 +300,6 @@ diff.git_tree <- function(x,
           as.integer(context_lines), as.integer(interhunk_lines),
           old_prefix, new_prefix, id_abbrev, path, max_size)
 }
+
+##' @export
+base::diff
